e9a49845b6222e98650a6ec2b4ba6b96c03a5839
Jay Malhotra <jay0@me.com>
2022-09-10 23:18:59 +0100

    Upgrade to .NET 6

--- DragaliaAPI/Program.cs
+++ DragaliaAPI/Program.cs
@@ -1,26 +1,29 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using DragaliaAPI.Models.Nintendo;
+using DragaliaAPI.Models;
+using MessagePack.AspNetCoreMvcFormatter;
+using MessagePack.Resolvers;
 
-namespace DragaliaAPI
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddMvc().AddMvcOptions(option =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    option.OutputFormatters.Add(new MessagePackOutputFormatter(ContractlessStandardResolver.Options));
+    option.InputFormatters.Add(new MessagePackInputFormatter(ContractlessStandardResolver.Options));
+});
+builder.Services.AddSingleton<ILoginFactory, LoginFactory>();
+builder.Services.AddSingleton<ISessionService, SessionService>();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
