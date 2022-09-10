599e0bcd846fb057f33f3f0a44784c290251b5e0
Jay Malhotra <jay0@me.com>
2022-09-10 23:18:59 +0100

    Upgrade to .NET 6

--- DragaliaAPI/GlobalSuppressions.cs
+++ DragaliaAPI/GlobalSuppressions.cs
@@ -5,4 +5,4 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "msgpack object fields should be start with lowercase to match real keys")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "msgpack/json object fields should start with lowercase to match real keys")]
