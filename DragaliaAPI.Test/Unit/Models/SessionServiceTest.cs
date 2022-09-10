b1769581a1375e68519bce43806e1a3a15d6d6d6
Jay Malhotra <jay0@me.com>
2022-09-10 23:33:41 +0100

    add sessionservice and tests

---
+++ DragaliaAPI.Test/Unit/Models/SessionServiceTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DragaliaAPI.Models;
+using DragaliaAPI.Models.Nintendo;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.DataCollection;
+using Xunit;
+
+namespace DragaliaAPI.Test.Unit.Models
+{
+    public class SessionServiceTest
+    {
+        private readonly SessionService sessionService = new();
+        private readonly DeviceAccount deviceAccount = new("id", "password");
+
+        [Fact]
+        public void CreateNewSession_NewSession_CreatesValidSession()
+        {
+            string sessionId = sessionService.CreateNewSession(deviceAccount);
+
+            sessionService.ValidateSession(deviceAccount, sessionId).Should().Be(true);
+        }
+
+        [Fact]
+        public void CreateNewSession_ExistingSession_ReplacesOldSession()
+        {
+            string firstSessionId = sessionService.CreateNewSession(deviceAccount);
+            string secondSessionId = sessionService.CreateNewSession(deviceAccount);
+
+            sessionService.ValidateSession(deviceAccount, secondSessionId).Should().Be(true);
+            secondSessionId.Should().NotBeEquivalentTo(firstSessionId);
+        }
+
+        [Fact]
+        public void ValidateSession_NonExistentSession_ReturnsFalse()
+        {
+            bool result = sessionService.ValidateSession(deviceAccount, "sessionId");
+            result.Should().Be(false);
+        }
+    }
+
+
+}
---
+++ DragaliaAPI/Models/SessionService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DragaliaAPI.Models.Nintendo;
+
+namespace DragaliaAPI.Models
+{
+    public class SessionService : ISessionService
+    {
+        private class Session
+        {
+            public string Id { get; set; }
+            public DeviceAccount deviceAccount { get; set; }
+
+            public Session(DeviceAccount deviceAccount)
+            {
+                Id = Guid.NewGuid().ToString();
+                this.deviceAccount = deviceAccount;
+            }
+        }
+
+        private List<Session> _sessions = new List<Session>();
+
+        public string CreateNewSession(DeviceAccount deviceAccount)
+        {
+            Session? existingSession = _sessions.SingleOrDefault(x => x.deviceAccount.id == deviceAccount.id);
+            if (existingSession != null)
+                _sessions.Remove(existingSession);
+            
+            Session session = new(deviceAccount);
+            _sessions.Add(session);
+
+            return session.Id;
+        }
+
+        public bool ValidateSession(DeviceAccount deviceAccount, string sessionId)
+        {
+            return _sessions.Any(x => x.deviceAccount.id == deviceAccount.id && x.Id == sessionId);
+        }
+    }
+}
