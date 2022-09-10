22d2930e88e1c1d58ed05f8cfc89e5f1b1f91602
Jay Malhotra <jay0@me.com>
2022-09-10 23:46:43 +0100

    switch LoginClasses to use records

--- DragaliaAPI/Controllers/Nintendo/WeatherForecastController.cs
+++ DragaliaAPI/Controllers/Nintendo/WeatherForecastController.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using DragaliaAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace DragaliaAPI.Controllers
+namespace DragaliaAPI.Controllers.Nintendo
 {
     [ApiController]
+    [Consumes("application/json")]
+    [Produces("application/json")]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
@@ -23,6 +26,7 @@ namespace DragaliaAPI.Controllers
         {
             _logger = logger;
         }
+
 
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
--- DragaliaAPI/Models/Nintendo/LoginClasses.cs
+++ DragaliaAPI/Models/Nintendo/LoginClasses.cs
@@ -3,119 +3,50 @@ using System.Text.Json.Serialization;
 
 namespace DragaliaAPI.Models.Nintendo
 {
-    public class DeviceAccount
+    public record DeviceAccount(string id, string? password);
+
+    // We only need the deviceAccount field from the request
+    public record LoginRequest(DeviceAccount? deviceAccount);
+
+    public record LoginResponse(string accessToken, string idToken, string sessionId, DeviceAccount deviceAccount)
     {
-        public string id { get; set; }
-        public string? password { get; set; }
+        public string behaviourSettings = "";
+        public Capability capability = new();
+        public DeviceAccount? createdDeviceAccount;
+        public string? error = null;
+        public int expiresIn = int.MaxValue;
+        public string? market = null;
+        public User user = new(deviceAccount);
 
-        [JsonConstructor]
-        public DeviceAccount(string id, string? password)
+        public record Capability()
         {
-            this.id = id;
-            this.password = password;
+            public string accountApiHost = "api.accounts.nintendo.com";
+            public string accountHost = "accounts.nintendo.com";
+            public string pointProgramHost = "my.nintendo.com";
+            public long sessionUpdateInterval = 180000;
         }
-    }
 
-    public class LoginRequest
-    {
-        public DeviceAccount? deviceAccount { get; set; }
-
-        [JsonConstructor]
-        public LoginRequest(DeviceAccount? deviceAccount)
+        public record User(DeviceAccount deviceAccount)
         {
-            this.deviceAccount = deviceAccount;
-        }
-    }
+            public string birthday = "0000-00-00";
+            public string country = "";
+            public long createdAt = 0;
+            public string gender = "Unknown";
+            public bool hasUnreadCsComment = false;
+            public string id = "";
+            public string links = "";
+            public string nickname = "";
+            public long updatedAt = 0;
+            public List<DeviceAccount> deviceAccounts = new() { deviceAccount };
+            public Permissions permissions = new();
 
-    // The constructor for this class fills in many irrelevant fields, and takes parameters for the important ones.
-    // It is intended to be called via a factory, so that DeviceAccounts can be validated against the DB.
-    public class LoginResponse
-    {
-        public string accessToken { get; set; }
-        public string behaviourSettings { get; set; }
-        public Capability capability { get; set; }
-        public DeviceAccount? createdDeviceAccount { get; set; }
-        public string? error { get; set; }
-        public int expiresIn { get; set; }
-        public string idToken { get; set; }
-        public string? market { get; set; }
-        public string sessionId { get; set; }
-        public User user { get; set; }
-
-        public class Capability
-        {
-            public string accountApiHost { get; set; }
-            public string accountHost { get; set; }
-            public string pointProgramHost { get; set; }
-            public long sessionUpdateInterval { get; set; }
-
-            public Capability()
+            public record Permissions
             {
-                accountApiHost = "api.accounts.nintendo.com";
-                accountHost = "accounts.nintendo.com";
-                pointProgramHost = "my.nintendo.com";
-                sessionUpdateInterval = 180000;
+                public bool personalAnalytics = false;
+                public long personalAnalyticsUpdatedAt = 0;
+                public bool personalNotification = false;
+                public long personalNotificationUpdatedAt = 0;
             }
-        }
-
-        public class User
-        {
-            public string birthday { get; set; }
-            public string country { get; set; }
-            public long createdAt { get; set; }
-            public string gender { get; set; }
-            public bool hasUnreadCsComment { get; set; }
-            public string id { get; set; }
-            public string links { get; set; }
-            public string nickname { get; set; }
-            public long updatedAt { get; set; }
-            public List<DeviceAccount>  deviceAccounts { get; set; }
-            public Permissions permissions { get; set; }
-
-            public class Permissions
-            {
-                public bool personalAnalytics { get; set; }
-                public long personalAnalyticsUpdatedAt { get; set; }
-                public bool personalNotification { get; set; }
-                public long personalNotificationUpdatedAt { get; set; }
-
-                public Permissions()
-                {
-                    this.personalAnalytics = false;
-                    this.personalAnalyticsUpdatedAt = 0;
-                    this.personalNotification = false;
-                    this.personalNotificationUpdatedAt = 0;
-                }
-            }
-
-            public User(DeviceAccount deviceAccount)
-            {
-                this.birthday = "0000-00-00";
-                this.country = "";
-                this.createdAt = 0;
-                this.gender = "Unknown";
-                this.hasUnreadCsComment = false;
-                this.id = "";
-                this.links = "";
-                this.nickname = "";
-                this.updatedAt = 0;
-                // This parameter is an array in the JSON response, despite the fact that it never seems to contain >1 account.
-                this.deviceAccounts = new() { deviceAccount };
-                this.permissions = new Permissions();
-            }
-        }
-
-        public LoginResponse(string accessToken, string idToken, string sessionId, DeviceAccount deviceAccount)
-        {
-            this.accessToken = accessToken;
-            this.behaviourSettings = "";
-            this.capability = new Capability();
-            this.error = null;
-            this.expiresIn = int.MaxValue;
-            this.idToken = idToken;
-            this.market = null;
-            this.sessionId = sessionId;
-            this.user = new User(deviceAccount);
         }
     }
 }
