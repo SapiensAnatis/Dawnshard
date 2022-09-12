70deb0f945b2ef165cf9bb962b778438c5d9ca9e
Jay Malhotra <jay0@me.com>
2022-09-12 23:47:48 +0100

    add redis database

--- DragaliaAPI.Test/Unit/Controllers/Nintendo/LoginTest.cs
+++ DragaliaAPI.Test/Unit/Controllers/Nintendo/LoginTest.cs
@@ -7,40 +7,67 @@ using Moq;
 using System.Text.Json;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
+using DragaliaAPI.Models;
 
 namespace DragaliaAPI.Test.Controllers.Nintendo
 {
     public class LoginTest
     {
-        private readonly Mock<ILogger<NintendoLoginController>> _mockLogger = new(MockBehavior.Strict);
-        private readonly Mock<ILoginService> _mockLoginService = new(MockBehavior.Strict);
+        private readonly Mock<ILogger<NintendoLoginController>> mockLogger = new(MockBehavior.Strict);
+        private readonly Mock<ISessionService> mockSessionService = new(MockBehavior.Strict);
+        private readonly Mock<IDeviceAccountService> mockDeviceAccountService = new(MockBehavior.Strict);
+        private NintendoLoginController nintendoLoginController;
+
+        private readonly DeviceAccount deviceAccount = new("test id", "test password");
 
         public LoginTest()
         {
-            
+            mockSessionService.Setup(x => x.CreateNewSession(deviceAccount, It.IsAny<string>())).Returns("session id");
+
+            nintendoLoginController = new(
+                mockLogger.Object,
+                mockSessionService.Object,
+                mockDeviceAccountService.Object);
         }
 
         [Fact]
-        public async Task LoginController_NullDeviceAccount_ReturnsCreatedDeviceAccount()
+        public async Task LoginController_NullDeviceAccount_ReturnsOKAndCreatedDeviceAccount()
         {
-            DeviceAccount deviceAccount = new("test id", "test password");
-
-            LoginResponse createDeviceAccountResponse = new("idToken", deviceAccount)
-            {
-                createdDeviceAccount = deviceAccount
-            };
-
-            _mockLoginService.Setup(x => x.DeviceAccountFactory()).ReturnsAsync(deviceAccount);
-            _mockLoginService.Setup(x => x.Login(deviceAccount)).ReturnsAsync(createDeviceAccountResponse);
-
+            mockDeviceAccountService.Setup(x => x.RegisterDeviceAccount()).ReturnsAsync(deviceAccount);
+            mockDeviceAccountService.Setup(x => x.AuthenticateDeviceAccount(deviceAccount)).ReturnsAsync(true);
             LoginRequest request = new(null);
-            NintendoLoginController nintendoLoginController = new(_mockLogger.Object, _mockLoginService.Object);
 
-            OkObjectResult? response = (await nintendoLoginController.Post(request)).Result as OkObjectResult;
+            ActionResult<LoginResponse> response = await nintendoLoginController.Post(request);
 
-            response.Should().NotBeNull();
-            response!.Value.Should().Be(createDeviceAccountResponse);
-            _mockLoginService.VerifyAll();
+            OkObjectResult goodResponse = Assert.IsType<OkObjectResult>(response.Result);
+            LoginResponse responseObject = Assert.IsType<LoginResponse>(goodResponse.Value);
+            responseObject.user.deviceAccounts.Should().BeEquivalentTo(new List<DeviceAccount> { deviceAccount });
+            responseObject.createdDeviceAccount.Should().BeEquivalentTo(deviceAccount);
+        }
+
+        [Fact]
+        public async Task LoginController_ExistingDeviceAccount_ReturnsOKAndDeviceAccount()
+        {
+            mockDeviceAccountService.Setup(x => x.AuthenticateDeviceAccount(deviceAccount)).ReturnsAsync(true);
+            LoginRequest request = new(deviceAccount);
+
+            ActionResult<LoginResponse> response = await nintendoLoginController.Post(request);
+
+            OkObjectResult goodResponse = Assert.IsType<OkObjectResult>(response.Result);
+            LoginResponse responseObject = Assert.IsType<LoginResponse>(goodResponse.Value);
+            responseObject.user.deviceAccounts.Should().BeEquivalentTo(new List<DeviceAccount> { deviceAccount });
+            responseObject.createdDeviceAccount.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task LoginController_DeviceAccountUnauthorized_ReturnsUnauthorized()
+        {
+            mockDeviceAccountService.Setup(x => x.AuthenticateDeviceAccount(deviceAccount)).ReturnsAsync(false);
+            LoginRequest request = new(deviceAccount);
+
+            ActionResult<LoginResponse> response = await nintendoLoginController.Post(request);
+
+            Assert.IsType<UnauthorizedResult>(response.Result);
         }
     }
 }
--- DragaliaAPI/Models/DeviceAccountService.cs
+++ DragaliaAPI/Models/DeviceAccountService.cs
@@ -1,25 +1,61 @@
-﻿using StackExchange.Redis;
+﻿using System.Security.Cryptography;
+using System.Text;
 using DragaliaAPI.Models.Nintendo;
+using Microsoft.Extensions.Caching.Distributed;
+
+
 
 namespace DragaliaAPI.Models
 {
     public class DeviceAccountService : IDeviceAccountService
     {
-        private readonly IConnectionMultiplexer _redis;
-
-        public DeviceAccountService(IConnectionMultiplexer redis)
+        private readonly IDistributedCache _cache;
+        private readonly ILogger<DeviceAccount> _logger;
+        private readonly IConfiguration _configuration;
+        public DeviceAccountService(IDistributedCache cache, IConfiguration configuration, ILogger<DeviceAccount> logger)
         {
-            _redis = redis;
+            _cache = cache;
+            _logger = logger;
+            _configuration = configuration;
         }
 
-        public Task<bool> AuthenticateDeviceAccount(DeviceAccount deviceAccount)
+        public async Task<bool> AuthenticateDeviceAccount(DeviceAccount deviceAccount)
         {
-            throw new NotImplementedException();
+            if (deviceAccount.password is null) { throw new ArgumentNullException(paramName: "deviceAccount.password"); }
+
+            string hashedPassword = GetHashedPassword(deviceAccount.password);
+            string storedPassword = await _cache.GetStringAsync($"DeviceAccount:{deviceAccount.id}:password");
+
+            return (hashedPassword == storedPassword);
         }
 
-        public Task<DeviceAccount> RegisterDeviceAccount()
+        public async Task<DeviceAccount> RegisterDeviceAccount()
         {
-            throw new NotImplementedException();
+            string id = Guid.NewGuid().ToString();
+            string password = Guid.NewGuid().ToString();
+
+            string hashedPassword = GetHashedPassword(password);
+
+            await _cache.SetStringAsync($"DeviceAccount:{id}:password", hashedPassword);
+
+            _logger.LogInformation("Registered new account with ID {id}", id);
+
+            return new DeviceAccount(id, password);
+            }
+
+        private string GetHashedPassword(string password)
+        {
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            // Dynamic salt would be better.
+            // But security is not a top priority for this application; it is unlikely to be publically hosted for mass use.
+            string salt = _configuration.GetValue<string>("HashSalt");
+            byte[] saltBytes = Encoding.UTF8.GetBytes(salt);
+
+            var pkbdf2 = new Rfc2898DeriveBytes(passwordBytes, saltBytes, 10000);
+            byte[] hashBytes = pkbdf2.GetBytes(20);
+            
+            return Convert.ToBase64String(hashBytes);
         }
     }
 }
