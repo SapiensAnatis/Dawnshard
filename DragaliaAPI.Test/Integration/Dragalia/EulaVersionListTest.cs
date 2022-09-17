4286ac27c7941a5c6a11ca99b2726f77dfbe01bc
Jay Malhotra <jay0@me.com>
2022-09-17 20:53:21 +0100

    eula endpoints

---
+++ DragaliaAPI.Test/Integration/Dragalia/EulaVersionListTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragaliaAPI.Test.Integration.Dragalia
+{
+    public class EulaVersionListTest : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+        private readonly CustomWebApplicationFactory<Program> _factory;
+
+        public EulaVersionListTest(CustomWebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+    }
+
+
+}
