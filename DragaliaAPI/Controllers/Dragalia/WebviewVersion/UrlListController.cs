2656e15bfc3a61b476c8ed2300911fc3dd9b02a8
Jay Malhotra <jay0@me.com>
2022-09-30 00:18:04 +0100

    Add url_list controller

---
+++ DragaliaAPI/Controllers/Dragalia/WebviewVersion/UrlListController.cs
@@ -0,0 +1,17 @@
+﻿using DragaliaAPI.Models.Dragalia.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DragaliaAPI.Controllers.Dragalia.WebviewVersion;
+[Route("webview_version/url_list")]
+[Consumes("application/octet-stream")]
+[Produces("application/octet-stream")]
+[ApiController]
+public class UrlListController : ControllerBase
+{
+    [HttpPost]
+    public DragaliaResult Post()
+    {
+        return Ok(new UrlListResponse());
+    } 
+}
---
+++ DragaliaAPI/Models/Dragalia/Responses/UrlListResponse.cs
@@ -0,0 +1,37 @@
+﻿namespace DragaliaAPI.Models.Dragalia.Responses;
+
+public record UrlListResponse : BaseResponse<UrlListData>
+{
+    public override UrlListData data { get; init; } = new(UrlListStaticData.AllUrls);
+}
+
+public record UrlListData(List<WebviewUrl> webview_url_list);
+
+public record WebviewUrl(string function_name, string url);
+
+public static class UrlListStaticData
+{
+    public static string PlaceholderUrl = "localhost";
+    public static List<WebviewUrl> AllUrls = new()
+    {
+        new("ability_crest_advice", PlaceholderUrl),
+        new("battle_royal_how_to", PlaceholderUrl),
+        new("comic", PlaceholderUrl),
+        new("plotsynopsis", PlaceholderUrl),
+        new("time_attack_ranking", PlaceholderUrl),
+        new("faq", PlaceholderUrl),
+        new("help_comic", PlaceholderUrl),
+        new("help", PlaceholderUrl),
+        new("information", PlaceholderUrl),
+        new("inquiry_attention", PlaceholderUrl),
+        new("dragon_battle_info", PlaceholderUrl),
+        new("quest_info", PlaceholderUrl),
+        new("copyright", PlaceholderUrl),
+        new("health", PlaceholderUrl),
+        new("payment_services_act", PlaceholderUrl),
+        new("privacy_policy", PlaceholderUrl),
+        new("specified_commercial_transactions_law", PlaceholderUrl),
+        new("user_policy", PlaceholderUrl),
+        new("summon_info", PlaceholderUrl),
+    };
+}
