13c1f0c13439468ca7f5fd57f914ffb15393a793
Jay Malhotra <jay0@me.com>
2022-10-05 21:06:05 +0100

    Add /redoable_summon/get_data

---
+++ DragaliaAPI/Controllers/Dragalia/RedoableSummon/GetDataController.cs
@@ -0,0 +1,19 @@
+﻿using DragaliaAPI.Models.Dragalia.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DragaliaAPI.Controllers.Dragalia.RedoableSummon;
+
+[Route("redoable_summon/get_data")]
+[Consumes("application/octet-stream")]
+[Produces("application/octet-stream")]
+[ApiController]
+public class GetDataController : ControllerBase
+{
+    [HttpPost]
+    public DragaliaResult Post()
+    {
+        RedoableSummonGetDataResponse response = new(RedoableSummonGetDataFactory.CreateData());
+        return Ok(response);
+    }
+}
---
+++ DragaliaAPI/Models/Dragalia/Responses/Common/SummonModels.cs
@@ -0,0 +1,37 @@
+﻿using MessagePack;
+
+namespace DragaliaAPI.Models.Dragalia.Responses.Common;
+
+[MessagePackObject(true)]
+public record SummonOddsData(SummonOdds normal, SummonOdds guarantee);
+
+[MessagePackObject(true)]
+public record SummonOdds(
+    List<Rarity> rarity_list,
+    List<RarityGroup> rarity_group_list,
+    OddsData unit
+);
+
+[MessagePackObject(true)]
+public record Rarity(int rarity, string total_rate);
+
+[MessagePackObject(true)]
+public record RarityGroup(
+    bool pickup,
+    int rarity,
+    string total_rate,
+    string chara_rate,
+    string dragon_rate
+);
+
+[MessagePackObject(true)]
+public record OddsData(List<RarityOddsList> chara_odds_list, List<RarityOddsList> dragon_odds_list);
+
+[MessagePackObject(true)]
+public record RarityOddsList(bool pickup, int rarity, List<Odds> unit_list);
+
+[MessagePackObject(true)]
+public record Odds(int id, string rate);
+
+[MessagePackObject(true)]
+public record SummonEntity(int entity_type, int id, int rarity);
