3d5610fdfafa52e810565cf5e43ca60efa2b465e
Jay Malhotra <jay0@me.com>
2022-10-05 21:06:21 +0100

    Add /redoable_summon/pre_exec

---
+++ DragaliaAPI/Controllers/Dragalia/RedoableSummon/PreExecController.cs
@@ -0,0 +1,20 @@
+﻿using DragaliaAPI.Models.Dragalia.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DragaliaAPI.Controllers.Dragalia.RedoableSummon;
+
+[Route("redoable_summon/pre_exec")]
+[Consumes("application/octet-stream")]
+[Produces("application/octet-stream")]
+[ApiController]
+public class PreExecController : ControllerBase
+{
+    [HttpPost]
+    public DragaliaResult Post()
+    {
+        // TODO: Make it actually execute a randomized summon
+        RedoableSummonPreExecResponse response = new(RedoableSummonPreExecFactory.CreateData());
+        return Ok(response);
+    }
+}
