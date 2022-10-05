4ad9d19cf09d8d3f9e1a9cefdf682084ba3993fe
Jay Malhotra <jay0@me.com>
2022-10-05 22:24:01 +0100

    Migrate database for TutorialFlag

---
+++ DragaliaAPI/Migrations/20221005203255_Alpha4.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DragaliaAPI.Migrations
+{
+    public partial class Alpha4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TutorialFlag",
+                table: "SavefileUserData",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "SavefileUserData",
+                keyColumn: "DeviceAccountId",
+                keyValue: "id",
+                column: "CreateTime",
+                value: 1665001975);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TutorialFlag",
+                table: "SavefileUserData");
+
+            migrationBuilder.UpdateData(
+                table: "SavefileUserData",
+                keyColumn: "DeviceAccountId",
+                keyValue: "id",
+                column: "CreateTime",
+                value: 1664719303);
+        }
+    }
+}
