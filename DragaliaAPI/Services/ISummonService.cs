b7f245da5536af696946792d366ed4349739ccad
Jay Malhotra <jay0@me.com>
2022-10-10 21:35:39 +0100

    Add SummonService

---
+++ DragaliaAPI/Services/ISummonService.cs
@@ -0,0 +1,8 @@
+﻿using DragaliaAPI.Models.Dragalia.Responses.Common;
+
+namespace DragaliaAPI.Services;
+
+public interface ISummonService
+{
+    List<SummonEntity> GenerateSummonResult(int numSummons);
+}
