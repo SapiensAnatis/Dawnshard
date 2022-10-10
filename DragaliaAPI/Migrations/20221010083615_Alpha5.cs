6b6a5ff4d0b6bda58f3ef26bd7d14b29238a9f70
Jay Malhotra <jay0@me.com>
2022-10-10 10:30:00 +0100

    Fix database in test fixture

---
+++ DragaliaAPI/Migrations/20221010083615_Alpha5.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DragaliaAPI.Migrations
+{
+    public partial class Alpha5 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "SavefileUserData",
+                keyColumn: "DeviceAccountId",
+                keyValue: "id",
+                column: "CreateTime",
+                value: 1665390975);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "SavefileUserData",
+                keyColumn: "DeviceAccountId",
+                keyValue: "id",
+                column: "CreateTime",
+                value: 1665001975);
+        }
+    }
+}
