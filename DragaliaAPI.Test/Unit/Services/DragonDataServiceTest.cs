c9681f7aafde8514ce8a4da21af5e914ba783d8e
Jay Malhotra <jay0@me.com>
2022-10-12 21:36:41 +0100

    Add DragonDataService

---
+++ DragaliaAPI.Test/Unit/Services/DragonDataServiceTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DragaliaAPI.Services.Data;
+using DragaliaAPI.Models.Data;
+
+namespace DragaliaAPI.Test.Unit.Services;
+
+public class DragonDataServiceTest
+{
+    private readonly IDragonDataService dragonDataService;
+
+    public DragonDataServiceTest()
+    {
+        this.dragonDataService = new DragonDataService();
+    }
+
+    [Fact]
+    public void Get_ReturnsCorrectData()
+    {
+        dragonDataService.GetData(Dragons.Agni).FullName.Should().Be("Agni");
+    }
+
+    [Fact]
+    public void Get_AllIdsValid()
+    {
+        foreach (Dragons d in Enum.GetValues<Dragons>())
+        {
+            dragonDataService.Invoking(x => x.GetData(d)).Should().NotThrow();
+        }
+    }
+
+    [Fact]
+    public void Get_InvalidId_ThrowsKeyNotFoundException()
+    {
+        dragonDataService.Invoking(x => x.GetData(0)).Should().Throw<KeyNotFoundException>();
+    }
+
+    [Fact]
+    public void Data_CanFilter()
+    {
+        dragonDataService.AllData
+            .Where(x => x.FullName == "Agni")
+            .Should()
+            .ContainSingle()
+            .And.ContainEquivalentOf(dragonDataService.GetData(Dragons.Agni));
+    }
+}
---
+++ DragaliaAPI/Services/Data/DragonDataService.cs
@@ -0,0 +1,34 @@
+﻿using DragaliaAPI.Models.Data;
+using DragaliaAPI.Services.Data.Models;
+using Newtonsoft.Json;
+
+namespace DragaliaAPI.Services.Data;
+
+// Tables to use for this CargoQuery:
+// Dragons.Id, Dragons.FullName, Dragons.Rarity, Dragons.ElementalType, Dragons.MinHp, Dragons.MaxHp, Dragons.AddMaxHp1, Dragons.MinAtk, Dragons.MaxAtk, Dragons.AddMaxAtk1, Dragons.SkillID, Dragons.Skill2ID, Dragons.Abilities11, Dragons.Abilities12, Dragons.Abilities13, Dragons.Abilities14, Dragons.Abilities15, Dragons.Abilities16, Dragons.Abilities21, Dragons.Abilities22, Dragons.Abilities23, Dragons.Abilities24, Dragons.Abilities25, Dragons.Abilities26, Dragons.DmodePassiveAbilityId, Dragons.MaxLimitBreakCount, Dragons.LimitBreakMaterialId, Dragons.FavoriteType, Dragons.SellCoin, Dragons.SellDewPoint, Dragons.Availability, Dragons.DraconicEssenceLocation,
+public class DragonDataService : IDragonDataService
+{
+    private const string _filename = "dragons.json";
+    private const string _folder = "Resources";
+
+    private readonly Dictionary<int, DataDragon> _dictionary;
+
+    public IEnumerable<DataDragon> AllData
+    {
+        get => _dictionary.Values;
+    }
+
+    public DragonDataService()
+    {
+        string json = File.ReadAllText(Path.Join(_folder, _filename));
+        List<DataDragon> deserialized = JsonConvert.DeserializeObject<List<DataDragon>>(json);
+
+        _dictionary = deserialized
+            .Select(x => new KeyValuePair<int, DataDragon>(x.Id, x))
+            .ToDictionary(x => x.Key, x => x.Value);
+    }
+
+    public DataDragon GetData(Dragons id) => _dictionary[(int)id];
+
+    public DataDragon GetData(int id) => _dictionary[id];
+}
