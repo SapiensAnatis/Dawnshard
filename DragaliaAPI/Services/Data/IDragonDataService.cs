cfb1170f5a225f3c98f18f8c59c8116259ea1629
Jay Malhotra <jay0@me.com>
2022-10-12 21:36:41 +0100

    Add DragonDataService

---
+++ DragaliaAPI/Services/Data/IDragonDataService.cs
@@ -0,0 +1,12 @@
+﻿using DragaliaAPI.Models.Data;
+using DragaliaAPI.Services.Data.Models;
+
+namespace DragaliaAPI.Services.Data;
+
+public interface IDragonDataService
+{
+    IEnumerable<DataDragon> AllData { get; }
+
+    DataDragon GetData(Dragons id);
+    DataDragon GetData(int id);
+}
