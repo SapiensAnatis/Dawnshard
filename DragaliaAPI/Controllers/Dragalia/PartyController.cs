e0f422888b9d4ec3f82a5ac5cc6621c7af557256
Jay Malhotra <jay0@me.com>
2022-10-19 21:10:05 +0100

    Add /party/index controller

---
+++ DragaliaAPI/Controllers/Dragalia/PartyController.cs
@@ -0,0 +1,22 @@
+﻿using DragaliaAPI.Models.Dragalia.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DragaliaAPI.Controllers.Dragalia;
+
+[Route("party")]
+[Consumes("application/octet-stream")]
+[Produces("application/octet-stream")]
+[ApiController]
+public class PartyController : ControllerBase
+{
+    /// <summary>
+    /// Does not seem to do anything useful.
+    /// ILSpy indicates the response should contain halidom info, but it is always empty and only called on fresh accounts.
+    /// </summary>
+    [Route("index")]
+    public DragaliaResult Index()
+    {
+        return Ok(new PartyIndexResponse(new PartyIndexData(new(), new())));
+    }
+}
