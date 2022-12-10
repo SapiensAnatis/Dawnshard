b8ac54a70dec9d9d2d91f323abc475f93939a40a
Jay Malhotra <jay0@me.com>
2022-12-10 15:55:07 +0000

    Automapper fixes

--- DragaliaAPI/Models/AutoMapper/UnitMapProfile.cs
+++ DragaliaAPI/Models/AutoMapper/UnitMapProfile.cs
@@ -9,15 +9,10 @@ public class UnitMapProfile : Profile
     public UnitMapProfile()
     {
         this.CreateMap<DbPlayerDragonData, DragonList>()
-            .ForMember(x => x.status_plus_count, opts => opts.Ignore())
-            .ReverseMap()
-            // Do not want to transport primary keys from official servers to my db
-            .ForMember(x => x.DragonKeyId, opts => opts.Ignore());
+            .ForMember(x => x.status_plus_count, opts => opts.Ignore());
 
         this.CreateMap<DbPlayerCharaData, CharaList>()
-            .ForMember(x => x.status_plus_count, opts => opts.Ignore())
-            .ReverseMap()
-            .ForMember(x => x.LimitBreakCount, opts => opts.Ignore());
+            .ForMember(x => x.status_plus_count, opts => opts.Ignore());
 
         this.CreateMap<DbPlayerDragonReliability, DragonReliabilityList>()
             .ForMember(
@@ -27,33 +22,28 @@ public class UnitMapProfile : Profile
             .ForMember(
                 nameof(DragonReliabilityList.reliability_total_exp),
                 o => o.MapFrom(src => src.Exp)
-            )
-            .ReverseMap();
+            );
 
         this.CreateMap<DbAbilityCrest, AbilityCrestList>()
             .ForMember(x => x.ability_1_level, opts => opts.Ignore())
-            .ForMember(x => x.ability_2_level, opts => opts.Ignore())
-            .ReverseMap();
+            .ForMember(x => x.ability_2_level, opts => opts.Ignore());
 
         this.CreateMap<DbAbilityCrest, GameAbilityCrest>()
-            // TODO: add actual mapping for this
-            .ForMember(x => x.ability_1_level, opts => opts.MapFrom(x => 0))
-            .ForMember(x => x.ability_2_level, opts => opts.MapFrom(x => 0))
-            .ReverseMap();
+            .ForMember(x => x.ability_1_level, opts => opts.Ignore())
+            .ForMember(x => x.ability_2_level, opts => opts.Ignore());
 
         this.CreateMap<DbWeaponBody, WeaponBodyList>()
             .ForMember(x => x.ability_1_level, opts => opts.Ignore())
             .ForMember(x => x.ability_2_levell, opts => opts.Ignore())
             .ForMember(x => x.skill_no, opts => opts.Ignore())
-            .ForMember(x => x.skill_level, opts => opts.Ignore())
-            .ReverseMap();
+            .ForMember(x => x.skill_level, opts => opts.Ignore());
 
         this.CreateMap<DbWeaponBody, GameWeaponBody>()
+            // TODO: actual mapping for this
             .ForMember(x => x.skill_no, opts => opts.MapFrom(x => 0))
             .ForMember(x => x.skill_level, opts => opts.MapFrom(x => 0))
             .ForMember(x => x.ability_1_level, opts => opts.MapFrom(x => 0))
-            .ForMember(x => x.ability_2_level, opts => opts.MapFrom(x => 0))
-            .ReverseMap();
+            .ForMember(x => x.ability_2_level, opts => opts.MapFrom(x => 0));
 
         this.CreateMap<DbDetailedPartyUnit, PartyUnitList>()
             .ForMember(x => x.weapon_skin_data, opts => opts.Ignore())
@@ -63,49 +53,15 @@ public class UnitMapProfile : Profile
             .ForMember(x => x.talisman_data, opts => opts.Ignore());
 
         this.CreateMap<DbParty, PartyList>()
-            .ForMember(nameof(PartyList.party_setting_list), opts => opts.MapFrom(x => x.Units))
-            .ReverseMap();
+            .ForMember(nameof(PartyList.party_setting_list), opts => opts.MapFrom(x => x.Units));
 
         this.CreateMap<DbPartyUnit, PartySettingList>()
-            // TODO: Why the fuck do I need to specify these manually
-            // Possible misunderst
-            .ForMember(
-                x => x.equip_crest_slot_type_1_crest_id_1,
-                opts => opts.MapFrom(x => x.EquipCrestSlotType1CrestId1)
-            )
-            .ForMember(
-                x => x.equip_crest_slot_type_1_crest_id_2,
-                opts => opts.MapFrom(x => x.EquipCrestSlotType1CrestId2)
-            )
-            .ForMember(
-                x => x.equip_crest_slot_type_1_crest_id_3,
-                opts => opts.MapFrom(x => x.EquipCrestSlotType1CrestId3)
-            )
-            .ForMember(
-                x => x.equip_crest_slot_type_2_crest_id_1,
-                opts => opts.MapFrom(x => x.EquipCrestSlotType2CrestId1)
-            )
-            .ForMember(
-                x => x.equip_crest_slot_type_2_crest_id_2,
-                opts => opts.MapFrom(x => x.EquipCrestSlotType2CrestId2)
-            )
-            .ForMember(
-                x => x.equip_crest_slot_type_3_crest_id_1,
-                opts => opts.MapFrom(x => x.EquipCrestSlotType2CrestId1)
-            )
-            .ForMember(
-                x => x.equip_crest_slot_type_3_crest_id_2,
-                opts => opts.MapFrom(x => x.EquipCrestSlotType2CrestId2)
-            )
             .ForMember(nameof(PartySettingList.equip_weapon_key_id), opts => opts.Ignore())
             .ForMember(nameof(PartySettingList.equip_amulet_key_id), opts => opts.Ignore())
             .ForMember(nameof(PartySettingList.equip_amulet_2_key_id), opts => opts.Ignore())
-            .ForMember(nameof(PartySettingList.equip_skin_weapon_id), opts => opts.Ignore())
-            .ReverseMap()
-            // Do not want to transport primary keys from official servers to my db
-            .ForMember(x => x.EquipDragonKeyId, opts => opts.MapFrom(src => 0));
+            .ForMember(nameof(PartySettingList.equip_skin_weapon_id), opts => opts.Ignore());
 
-        this.SourceMemberNamingConvention = new PascalCaseNamingConvention();
-        this.DestinationMemberNamingConvention = new LowerUnderscoreNamingConvention();
+        this.SourceMemberNamingConvention = PascalCaseNamingConvention.Instance;
+        this.DestinationMemberNamingConvention = LowerUnderscoreNamingConvention.Instance;
     }
 }
---
+++ DragaliaAPI/Models/AutoMapper/UnitReverseMapProfile.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using DragaliaAPI.Database.Entities;
+using DragaliaAPI.Models.Generated;
+
+namespace DragaliaAPI.Models.AutoMapper;
+
+public class UnitReverseMapProfile : Profile
+{
+    public UnitReverseMapProfile()
+    {
+        this.AddGlobalIgnore("DeviceAccount");
+
+        this.CreateMap<DragonList, DbPlayerDragonData>()
+            .ForMember(x => x.DragonKeyId, opts => opts.Ignore());
+
+        this.CreateMap<CharaList, DbPlayerCharaData>()
+            .ForMember(x => x.LimitBreakCount, opts => opts.Ignore());
+
+        this.CreateMap<DragonReliabilityList, DbPlayerDragonReliability>()
+            .ForMember(x => x.Level, opts => opts.MapFrom(src => src.reliability_level))
+            .ForMember(x => x.Exp, opts => opts.MapFrom(src => src.reliability_total_exp));
+
+        this.CreateMap<AbilityCrestList, DbAbilityCrest>();
+
+        this.CreateMap<WeaponBodyList, DbWeaponBody>();
+
+        this.CreateMap<PartyList, DbParty>()
+            .ForMember(x => x.Units, opts => opts.MapFrom(src => src.party_setting_list));
+
+        this.CreateMap<PartySettingList, DbPartyUnit>()
+            // Do not want to transport primary keys from official servers to my db
+            .ForMember(x => x.EquipDragonKeyId, opts => opts.MapFrom(src => 0))
+            .ForMember(x => x.EquipTalismanKeyId, opts => opts.MapFrom(src => 0))
+            // Auto-generated primary key
+            .ForMember(x => x.Id, opts => opts.Ignore())
+            // Foreign keys
+            .ForMember(x => x.PartyNo, opts => opts.Ignore())
+            .ForMember(x => x.Party, opts => opts.Ignore());
+
+        this.SourceMemberNamingConvention = LowerUnderscoreNamingConvention.Instance;
+        this.DestinationMemberNamingConvention = DatabaseNamingConvention.Instance;
+    }
+}
