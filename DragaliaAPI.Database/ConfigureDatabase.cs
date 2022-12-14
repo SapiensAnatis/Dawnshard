be3f4dd124f9a1226e4edac81d2c7a5ec94f4030
Jay Malhotra <jay0@me.com>
2022-12-14 22:04:47 +0000

    Migrate to PostgreSQL (#33)
    Migrating to PostgreSQL from SQL Server due to licensing concerns.
    
    Additionally reconfigures the way database passwords are handled: 
    - Previously these were in appsettings.json and the docker-compose.yml
    file, which not only left them in plaintext and presented a security
    vulnerability if unchanged, but also presented two sources of truth.
    - With this PR, they are now derived from environment variables
    specified in postgres.env which is left blank by default. This file is
    in the .gitignore so that database password will not accidentally be
    committed.

--- DragaliaAPI.Database/ConfigureDatabase.cs
+++ DragaliaAPI.Database/ConfigureDatabase.cs
@@ -1,7 +1,9 @@
 ﻿using DragaliaAPI.Database.Repositories;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Npgsql;
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("DragaliaAPI.Database.Test")]
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("DragaliaAPI.Test")]
@@ -10,15 +12,15 @@ namespace DragaliaAPI.Database;
 
 public static class DatabaseConfiguration
 {
+    private const int MigrationMaxRetries = 5;
+
     public static IServiceCollection ConfigureDatabaseServices(
         this IServiceCollection services,
-        IConfiguration configuration
+        string? host
     )
     {
         services = services
-            .AddDbContext<ApiContext>(
-                options => options.UseSqlServer(configuration.GetConnectionString("SqlConnection"))
-            )
+            .AddDbContext<ApiContext>(options => options.UseNpgsql(GetConnectionString(host)))
             .AddScoped<IDeviceAccountRepository, DeviceAccountRepository>()
             .AddScoped<IUserDataRepository, UserDataRepository>()
             .AddScoped<IUnitRepository, UnitRepository>()
@@ -29,5 +31,47 @@ public static class DatabaseConfiguration
             .AddScoped<IInventoryRepository, InventoryRepository>();
 
         return services;
+    }
+
+    public static string GetConnectionString(string? host)
+    {
+        NpgsqlConnectionStringBuilder connectionStringBuilder =
+            new()
+            {
+                Host = host ?? "postgres",
+                Database = "database",
+                Username = Environment.GetEnvironmentVariable("POSTGRES_USER"),
+                Password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD")
+            };
+
+        return connectionStringBuilder.ConnectionString;
+    }
+
+    public static void MigrateDatabase(this WebApplication app)
+    {
+        using IServiceScope scope = app.Services
+            .GetRequiredService<IServiceScopeFactory>()
+            .CreateScope();
+
+        ApiContext context = scope.ServiceProvider.GetRequiredService<ApiContext>();
+
+        if (!context.Database.IsRelational())
+            return;
+
+        int tries = 0;
+        while (!context.Database.CanConnect())
+        {
+            tries++;
+            if (tries >= MigrationMaxRetries)
+            {
+                throw new InvalidOperationException(
+                    $"Failed to apply database migrations: could not connect to database after {tries} attempts."
+                );
+            }
+
+            Thread.Sleep(1000);
+        }
+
+        context.Database.Migrate();
     }
 }
