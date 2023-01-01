4fa449e60949d0acb310aa711553120159b69a8d
Jay Malhotra <jay0@me.com>
2023-01-01 07:29:51 +0000

    Make news page design more responsive

--- DragaliaAPI/Pages/NewsModel.cs
+++ DragaliaAPI/Pages/NewsModel.cs
@@ -7,7 +7,8 @@ namespace DragaliaAPI.Pages;
 
 public record NewsItem(string Headline, string Description, long Timestamp)
 {
-    public string Date => $"{DateTimeOffset.FromUnixTimeSeconds(this.Timestamp):G} UTC";
+    public string Date =>
+        $"{DateTimeOffset.FromUnixTimeSeconds(this.Timestamp):dd/MM/yyyy HH:mm} UTC";
 }
 
 public class NewsModel : PageModel
