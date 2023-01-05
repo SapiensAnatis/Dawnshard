c88debd6c96964126a81623c726835b327782076
Jay Malhotra <jay0@me.com>
2023-01-05 23:17:44 +0000

    Add FortPlantDetail to masterasset

--- DragaliaAPI.Shared/MasterAsset/MasterAsset.cs
+++ DragaliaAPI.Shared/MasterAsset/MasterAsset.cs
@@ -7,21 +7,21 @@ public static class MasterAsset
 {
     private static readonly Lazy<MasterAssetData<Charas, CharaData>> _CharaData =
         new(() => new("CharaData.json", x => x.Id));
-
     public static MasterAssetData<Charas, CharaData> CharaData => _CharaData.Value;
 
     private static readonly Lazy<MasterAssetData<Dragons, DragonData>> _DragonData =
         new(() => new("DragonData.json", x => x.Id));
-
     public static MasterAssetData<Dragons, DragonData> DragonData => _DragonData.Value;
 
     private static readonly Lazy<MasterAssetData<int, ManaNode>> _ManaNode =
         new(() => new("MC.json", x => x.MC_0));
-
     public static MasterAssetData<int, ManaNode> ManaNode => _ManaNode.Value;
 
     private static readonly Lazy<MasterAssetData<int, QuestData>> _QuestData =
         new(() => new("QuestData.json", x => x.Id));
-
     public static MasterAssetData<int, QuestData> QuestData => _QuestData.Value;
+
+    private static readonly Lazy<MasterAssetData<int, FortPlant>> _FortPlant =
+        new(() => new("FortPlantDetail.json", x => x.Id));
+    public static MasterAssetData<int, FortPlant> FortPlant => _FortPlant.Value;
 }
---
+++ DragaliaAPI.Shared/MasterAsset/Models/FortPlant.cs
@@ -0,0 +1,34 @@
+﻿using DragaliaAPI.Shared.Definitions.Enums;
+
+namespace DragaliaAPI.Shared.MasterAsset.Models;
+
+public record FortPlant(
+    int Id,
+    int AssetGroup,
+    int Level,
+    int LevelType,
+    int NeedLevel,
+    int Time,
+    int Cost,
+    Materials MaterialsId1,
+    int MaterialsNum1,
+    Materials MaterialsId2,
+    int MaterialsNum2,
+    Materials MaterialsId3,
+    int MaterialsNum3,
+    Materials MaterialsId4,
+    int MaterialsNum4,
+    Materials MaterialsId5,
+    int MaterialsNum5,
+    int EffectId,
+    int EffType1,
+    int EffType2,
+    double EffArgs1,
+    double EffArgs2,
+    int MaterialMaxTime,
+    int MaterialMax,
+    int StaminaMaxTime,
+    int StaminaMax,
+    int EventEffectType,
+    double EventEffectArgs
+);
