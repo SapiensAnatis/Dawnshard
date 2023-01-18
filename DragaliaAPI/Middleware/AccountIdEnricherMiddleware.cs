2077fd2fcc7e12965fc0b4470455087ce6ef7235
Jay Malhotra <jay0@me.com>
2023-01-18 14:34:12 +0000

    Improve logging (#92)
    - Add middleware to inject DeviceAccountId into all logs during a
    request's lifetime (controllers, DB errors, etc) instead of just the
    request logging at the end
    - Fix a typo causing save import errors to not log the exception

---
+++ DragaliaAPI/Middleware/AccountIdEnricherMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Claims;
+using Serilog.Context;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace DragaliaAPI.Middleware;
+
+public class AccountIdEnricherMiddleware
+{
+    private readonly RequestDelegate next;
+    private readonly ILogger logger;
+
+    public AccountIdEnricherMiddleware(RequestDelegate next, ILoggerFactory logger)
+    {
+        this.next = next;
+        this.logger = logger.CreateLogger<ExceptionHandlerMiddleware>();
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        string? accountId = context.User.FindFirstValue(CustomClaimType.AccountId);
+        using (LogContext.PushProperty(CustomClaimType.AccountId, accountId ?? "anonymous"))
+        {
+            await this.next(context);
+        }
+    }
+}
