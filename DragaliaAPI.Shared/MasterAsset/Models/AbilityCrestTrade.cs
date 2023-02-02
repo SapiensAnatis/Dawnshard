936c3cfec7a5af8dcd734b3415466ab82b9bf423
Nightmerp <43102229+Nightmerp@users.noreply.github.com>
2023-02-02 15:10:08 -0800

    Added AbilityCrestTradeController and AbilityCrestTrade data

---
+++ DragaliaAPI.Shared/MasterAsset/Models/AbilityCrestTrade.cs
@@ -0,0 +1,11 @@
+﻿using DragaliaAPI.Shared.Definitions.Enums;
+
+namespace DragaliaAPI.Shared.MasterAsset.Models;
+
+public record AbilityCrestTrade(
+    int Id,
+    AbilityCrests AbilityCrestId,
+    int NeedDewPoint,
+    int Priority,
+    int LabelType
+);
---
+++ DragaliaAPI/Controllers/Dragalia/AbilityCrestTradeController.cs
@@ -0,0 +1,34 @@
+﻿using DragaliaAPI.Database.Repositories;
+using DragaliaAPI.Models.Generated;
+using DragaliaAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DragaliaAPI.Controllers.Dragalia;
+
+[Route("ability_crest_trade")]
+public class AbilityCrestTradeController : DragaliaControllerBase
+{
+    private readonly IUserDataRepository userDataRepository;
+    private readonly IUnitRepository unitRepository;
+    private readonly IUpdateDataService updateDataService;
+
+    public AbilityCrestTradeController(
+        IUserDataRepository userDataRepository,
+        IUnitRepository unitRepository,
+        IUpdateDataService updateDataService
+    )
+    {
+        this.userDataRepository = userDataRepository;
+        this.unitRepository = unitRepository;
+        this.updateDataService = updateDataService;
+    }
+
+    [Route("get_list")]
+    [HttpPost]
+    public async Task<DragaliaResult> GetList(AbilityCrestTradeGetListRequest request)
+    {
+        AbilityCrestTradeGetListData response = new();
+
+        return Ok(response);
+    }
+}
