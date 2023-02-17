dd26fe713d74907930123bf0a00fc1961073349b
Jay Malhotra <jay0@me.com>
2023-02-17 23:52:45 +0000

    Update tests

---
+++ DragaliaAPI.Test/Unit/Services/QuestRewardServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using DragaliaAPI.Services;
+using DragaliaAPI.Shared.Definitions.Enums;
+using DragaliaAPI.Test.Utils;
+
+namespace DragaliaAPI.Test.Unit.Services;
+
+public class QuestRewardServiceTest
+{
+    private readonly IQuestRewardService questRewardService;
+
+    public QuestRewardServiceTest()
+    {
+        this.questRewardService = new QuestRewardService(
+            LoggerTestUtils.Create<QuestRewardService>()
+        );
+    }
+
+    [Fact]
+    public void QuestRewardService_GetDrops_Defined_ReturnsExpectedResult()
+    {
+        this.questRewardService
+            .GetDrops(219011101)
+            .Should()
+            .BeEquivalentTo(
+                new List<Materials>()
+                {
+                    Materials.AmplifyingCrystal,
+                    Materials.AmplifyingGemstone,
+                    Materials.ConsecratedWater,
+                    Materials.DestituteOnesMaskFragment,
+                    Materials.FortifyingCrystal,
+                    Materials.FortifyingGemstone
+                }
+            );
+    }
+
+    [Fact]
+    public void QuestRewardService_GetDrops_Undefined_ReturnsEmpty()
+    {
+        this.questRewardService.GetDrops(4).Should().BeEquivalentTo(new List<Materials>());
+    }
+}
