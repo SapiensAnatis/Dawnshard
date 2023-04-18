00c4594b2a983bcb67f514096b6d6f8dd064b86a
Nightmerp <43102229+Nightmerp@users.noreply.github.com>
2023-04-17 17:31:28 -0700

    Integration tests

--- DragaliaAPI.Test/Integration/Dragalia/AbilityCrestTest.cs
+++ DragaliaAPI.Test/Integration/Dragalia/AbilityCrestTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using DragaliaAPI.Database;
+﻿using DragaliaAPI.Database;
 using DragaliaAPI.Database.Entities;
 using DragaliaAPI.Database.Repositories;
 using DragaliaAPI.Models;
@@ -9,7 +8,6 @@ using DragaliaAPI.Shared.MasterAsset.Models;
 using DragaliaAPI.Test.Utils;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 
 namespace DragaliaAPI.Test.Integration.Dragalia;
 
@@ -68,23 +66,193 @@ public class AbilityCrestTest : IClassFixture<IntegrationTestFixture>
     }
 
     [Fact]
-    public async Task SetFavorite_ThrowsExceptionWhenAbilityCrestNotFound()
+    public async Task SetFavorite_ReturnsErrorWhenAbilityCrestNotFound()
     {
-        try
-        {
-            await client.PostMsgpack<AbilityCrestSetFavoriteData>(
+        ResultCodeData data = (
+            await client.PostMsgpack<ResultCodeData>(
                 "ability_crest/set_favorite",
                 new AbilityCrestSetFavoriteRequest()
                 {
-                    ability_crest_id = AbilityCrests.ManaFount,
+                    ability_crest_id = AbilityCrests.SweetSurprise,
                     is_favorite = true
+                },
+                ensureSuccessHeader: false
+            )
+        ).data;
+
+        data.result_code.Should().Be(ResultCode.CommonInvalidArgument);
+    }
+
+    [Fact]
+    public async Task BuildupPiece_ReturnsErrorWhenAbilityCrestNotFound()
+    {
+        ResultCodeData data = (
+            await client.PostMsgpack<ResultCodeData>(
+                "ability_crest/buildup_piece",
+                new AbilityCrestBuildupPieceRequest()
+                {
+                    ability_crest_id = AbilityCrests.InanUnendingWorld,
+                    buildup_ability_crest_piece_list = new List<AtgenBuildupAbilityCrestPieceList>()
+                    {
+                        new()
+                        {
+                            buildup_piece_type = BuildupPieceTypes.Unbind,
+                            is_use_dedicated_material = true,
+                            step = 1
+                        }
+                    }
+                },
+                ensureSuccessHeader: false
+            )
+        ).data;
+
+        data.result_code.Should().Be(ResultCode.AbilityCrestBuildupPieceUnablePiece);
+    }
+
+    [Fact]
+    public async Task BuildupPiece_InvalidStepsReturnsErrorAndDoesntAffectDatabase()
+    {
+        this.fixture.ApiContext.PlayerAbilityCrests.Add(
+            new DbAbilityCrest()
+            {
+                DeviceAccountId = fixture.DeviceAccountId,
+                AbilityCrestId = AbilityCrests.HappyNewYear
+            }
+        );
+
+        await this.fixture.ApiContext.SaveChangesAsync();
+
+        ResultCodeData data = (
+            await client.PostMsgpack<ResultCodeData>(
+                "ability_crest/buildup_piece",
+                new AbilityCrestBuildupPieceRequest()
+                {
+                    ability_crest_id = AbilityCrests.HappyNewYear,
+                    buildup_ability_crest_piece_list = new List<AtgenBuildupAbilityCrestPieceList>()
+                    {
+                        new()
+                        {
+                            buildup_piece_type = BuildupPieceTypes.Unbind,
+                            is_use_dedicated_material = true,
+                            step = 1
+                        },
+                        new()
+                        {
+                            buildup_piece_type = BuildupPieceTypes.Unbind,
+                            is_use_dedicated_material = true,
+                            step = 4
+                        }
+                    }
+                },
+                ensureSuccessHeader: false
+            )
+        ).data;
+
+        DbAbilityCrest ability_crest = (
+            await this.fixture.ApiContext.PlayerAbilityCrests.FindAsync(
+                fixture.DeviceAccountId,
+                AbilityCrests.HappyNewYear
+            )
+        )!;
+        await this.fixture.ApiContext.Entry(ability_crest).ReloadAsync();
+
+        data.result_code.Should().Be(ResultCode.AbilityCrestBuildupPieceStepError);
+        ability_crest.LimitBreakCount.Should().Be(0);
+    }
+
+    [Fact]
+    public async Task BuildupPiece_SuccessDecreasesMaterialsAndUpdatesDatabase()
+    {
+        this.fixture.ApiContext.PlayerAbilityCrests.Add(
+            new DbAbilityCrest()
+            {
+                DeviceAccountId = fixture.DeviceAccountId,
+                AbilityCrestId = AbilityCrests.WorthyRivals
+            }
+        );
+
+        await this.fixture.ApiContext.SaveChangesAsync();
+
+        int oldDewpoint = this.GetDewpoint();
+        int oldGoldenKey = this.GetMaterial(Materials.GoldenKey);
+        int oldHolyWater = this.GetMaterial(Materials.HolyWater);
+        int oldConsecratedWater = this.GetMaterial(Materials.ConsecratedWater);
+
+        await client.PostMsgpack<AbilityCrestBuildupPieceData>(
+            "ability_crest/buildup_piece",
+            new AbilityCrestBuildupPieceRequest()
+            {
+                ability_crest_id = AbilityCrests.WorthyRivals,
+                buildup_ability_crest_piece_list = new List<AtgenBuildupAbilityCrestPieceList>()
+                {
+                    new()
+                    {
+                        buildup_piece_type = BuildupPieceTypes.Unbind,
+                        is_use_dedicated_material = true,
+                        step = 2
+                    },
+                    new()
+                    {
+                        buildup_piece_type = BuildupPieceTypes.Unbind,
+                        is_use_dedicated_material = false,
+                        step = 1
+                    },
+                    new()
+                    {
+                        buildup_piece_type = BuildupPieceTypes.Stats,
+                        is_use_dedicated_material = false,
+                        step = 3
+                    },
+                    new()
+                    {
+                        buildup_piece_type = BuildupPieceTypes.Stats,
+                        is_use_dedicated_material = false,
+                        step = 2
+                    },
+                    new()
+                    {
+                        buildup_piece_type = BuildupPieceTypes.Copies,
+                        is_use_dedicated_material = false,
+                        step = 2
+                    }
                 }
-            );
-            Assert.True(false);
-        }
-        catch (Exception)
-        {
-            Assert.True(true);
-        }
+            }
+        );
+
+        this.GetDewpoint().Should().Be(oldDewpoint - 43_000);
+        this.GetMaterial(Materials.GoldenKey).Should().Be(oldGoldenKey - 1);
+        this.GetMaterial(Materials.HolyWater).Should().Be(oldHolyWater - 6);
+        this.GetMaterial(Materials.ConsecratedWater).Should().Be(oldConsecratedWater - 10);
+
+        DbAbilityCrest ability_crest = (
+            await this.fixture.ApiContext.PlayerAbilityCrests.FindAsync(
+                fixture.DeviceAccountId,
+                AbilityCrests.WorthyRivals
+            )
+        )!;
+        await this.fixture.ApiContext.Entry(ability_crest).ReloadAsync();
+
+        ability_crest.LimitBreakCount.Should().Be(2);
+        ability_crest.AbilityLevel.Should().Be(2);
+        ability_crest.BuildupCount.Should().Be(3);
+        ability_crest.EquipableCount.Should().Be(2);
+    }
+
+    private int GetDewpoint()
+    {
+        return this.fixture.ApiContext.PlayerUserData
+            .AsNoTracking()
+            .Where(x => x.DeviceAccountId == fixture.DeviceAccountId)
+            .Select(x => x.DewPoint)
+            .First();
+    }
+
+    private int GetMaterial(Materials materialId)
+    {
+        return this.fixture.ApiContext.PlayerMaterials
+            .AsNoTracking()
+            .Where(x => x.DeviceAccountId == fixture.DeviceAccountId && x.MaterialId == materialId)
+            .Select(x => x.Quantity)
+            .First();
     }
 }
