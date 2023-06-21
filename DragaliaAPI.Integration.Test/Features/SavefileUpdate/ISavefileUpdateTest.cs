5908674ca77ecbf31c253156478f6cace825087b
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2023-06-21 22:04:51 +0100

    Remove dodgy test

--- DragaliaAPI.Integration.Test/Features/SavefileUpdate/ISavefileUpdateTest.cs
+++ DragaliaAPI.Integration.Test/Features/SavefileUpdate/ISavefileUpdateTest.cs
@@ -18,24 +18,6 @@ public class ISavefileUpdateTest : TestFixture
     }
 
     [Fact]
-    public async Task ISavefileUpdate_Apply_CanBeAppliedTwice()
-    {
-        // Check that none of the savefile updates cause PK errors when writing over duplicate data.
-
-        IPlayerIdentityService identityService =
-            this.Services.GetRequiredService<IPlayerIdentityService>();
-        using IDisposable ctx = identityService.StartUserImpersonation(DeviceAccountId);
-
-        foreach (ISavefileUpdate update in updates)
-        {
-            await update.Apply();
-            await this.ApiContext.SaveChangesAsync();
-
-            await update.Invoking(x => x.Apply()).Should().NotThrowAsync();
-        }
-    }
-
-    [Fact]
     public void ISavefileUpdate_HasExpectedCount()
     {
         // Update this test when adding a new update.
