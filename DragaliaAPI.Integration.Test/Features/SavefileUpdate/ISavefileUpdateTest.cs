5a0b022e32268a96f3a4490e2ae4ac245c84bca7
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2023-06-28 01:05:31 +0200

    Add savefile update for fixing fort levelups

--- DragaliaAPI.Integration.Test/Features/SavefileUpdate/ISavefileUpdateTest.cs
+++ DragaliaAPI.Integration.Test/Features/SavefileUpdate/ISavefileUpdateTest.cs
@@ -21,7 +21,7 @@ public class ISavefileUpdateTest : TestFixture
     public void ISavefileUpdate_HasExpectedCount()
     {
         // Update this test when adding a new update.
-        this.updates.Should().HaveCount(5);
+        this.updates.Should().HaveCount(6);
     }
 
     [Fact]
---
+++ DragaliaAPI/Features/SavefileUpdate/V6Update.cs
@@ -0,0 +1,33 @@
+﻿using DragaliaAPI.Database.Entities;
+using DragaliaAPI.Database.Repositories;
+using DragaliaAPI.Services;
+using DragaliaAPI.Shared.Definitions.Enums;
+using DragaliaAPI.Shared.MasterAsset;
+using Microsoft.EntityFrameworkCore;
+
+namespace DragaliaAPI.Features.SavefileUpdate;
+
+public class V6Update : ISavefileUpdate
+{
+    private readonly IFortRepository fortRepository;
+
+    public V6Update(IFortRepository fortRepository)
+    {
+        this.fortRepository = fortRepository;
+    }
+
+    public int SavefileVersion => 6;
+
+    public async Task Apply()
+    {
+        foreach (
+            DbFortBuild build in (await this.fortRepository.Builds.ToListAsync()).Where(
+                x => x.BuildStatus == FortBuildStatus.LevelUp
+            )
+        )
+        {
+            if (!MasterAsset.FortPlant.TryGetValue(build.FortPlantDetailId, out _))
+                build.Level--;
+        }
+    }
+}
