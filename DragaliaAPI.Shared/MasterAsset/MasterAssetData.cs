b7011c70bad10bac69017079d2233bd2243a631b
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2023-06-27 23:19:01 +0200

    make InternalKeyedCollection private again

--- DragaliaAPI.Shared/MasterAsset/MasterAssetData.cs
+++ DragaliaAPI.Shared/MasterAsset/MasterAssetData.cs
@@ -92,7 +92,7 @@ public class MasterAssetData<TKey, TItem>
         return result;
     }
 
-    internal class InternalKeyedCollection : KeyedCollection<TKey, TItem>
+    private class InternalKeyedCollection : KeyedCollection<TKey, TItem>
     {
         private readonly Func<TItem, TKey> keySelector;
 
