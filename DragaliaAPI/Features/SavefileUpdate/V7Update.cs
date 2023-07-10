e23ec2e3aafff9a06945b5f5785c1130e6bb8c27
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2023-07-11 01:02:07 +0200

    add pdt tutorial flag and update

---
+++ DragaliaAPI/Features/SavefileUpdate/V7Update.cs
@@ -0,0 +1,18 @@
+﻿using DragaliaAPI.Database.Repositories;
+using DragaliaAPI.Services;
+
+namespace DragaliaAPI.Features.SavefileUpdate;
+
+public class V7Update(IStoryRepository storyRepository, ITutorialService tutorialService)
+    : ISavefileUpdate
+{
+    public int SavefileVersion => 7;
+
+    public async Task Apply()
+    {
+        if (await storyRepository.HasReadQuestStory(1001610))
+        {
+            await tutorialService.AddTutorialFlag(1006);
+        }
+    }
+}
