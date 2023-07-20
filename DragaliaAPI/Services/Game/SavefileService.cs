48509a6ce0d9de43e6a341e97da5aa530b523574
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2023-07-20 20:22:43 +0100

    Remove default savefile items (#312)
    When creating a new savefile, you are given a set of dragons, prints,
    and weapons with maxed out stats. This is a historical workaround to
    missing systems to obtain/upgrade these entities.
    
    With these systems now being implemented, there is no reason to grant
    these items.
    
    - Add all 3* characters by default as was done on official servers

--- DragaliaAPI/Services/Game/SavefileService.cs
+++ DragaliaAPI/Services/Game/SavefileService.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
 using AutoMapper;
 using DragaliaAPI.Database;
 using DragaliaAPI.Database.Entities;
@@ -7,6 +8,7 @@ using DragaliaAPI.Features.Stamp;
 using DragaliaAPI.Models.Generated;
 using DragaliaAPI.Models.Nintendo;
 using DragaliaAPI.Shared.Definitions.Enums;
+using DragaliaAPI.Shared.MasterAsset;
 using DragaliaAPI.Shared.PlayerDetails;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
@@ -558,7 +560,7 @@ public class SavefileService : ISavefileService
             .AsSplitQuery();
     }
 
-    public async Task CreateBase()
+    public async Task Create()
     {
         string deviceAccountId = this.playerIdentityService.AccountId;
 
@@ -592,21 +594,6 @@ public class SavefileService : ISavefileService
         await transaction.CommitAsync();
     }
 
-    public async Task Create()
-    {
-        await this.CreateBase();
-
-        string deviceAccountId = this.playerIdentityService.AccountId;
-
-        await this.AddDefaultWyrmprints(deviceAccountId);
-        await this.AddDefaultDragons(deviceAccountId);
-        await this.AddDefaultWeapons(deviceAccountId);
-        await this.AddDefaultMaterials(deviceAccountId);
-
-        await this.apiContext.SaveChangesAsync();
-    }
-
-    #region Default save data
     private async Task AddDefaultParties(string deviceAccountId)
     {
         await this.apiContext.PlayerParties.AddRangeAsync(
@@ -686,318 +673,16 @@ public class SavefileService : ISavefileService
         );
     }
 
-    private async Task AddDefaultWyrmprints(string deviceAccountId)
-    {
-        await this.apiContext.PlayerAbilityCrests.AddRangeAsync(
-            DefaultSavefileData.FiveStarCrests
-                .Select(
-                    x =>
-                        new DbAbilityCrest()
-                        {
-                            AbilityCrestId = x,
-                            BuildupCount = 50,
-                            LimitBreakCount = 4,
-                            DeviceAccountId = deviceAccountId,
-                            AttackPlusCount = 50,
-                            HpPlusCount = 50,
-                            EquipableCount = 4,
-                            GetTime = DateTimeOffset.UtcNow,
-                            IsFavorite = false,
-                            IsNew = false,
-                        }
-                )
-                .Concat(
-                    DefaultSavefileData.FourStarCrests.Select(
-                        x =>
-                            new DbAbilityCrest()
-                            {
-                                AbilityCrestId = x,
-                                BuildupCount = 40,
-                                LimitBreakCount = 4,
-                                DeviceAccountId = deviceAccountId,
-                                AttackPlusCount = 50,
-                                HpPlusCount = 50,
-                                EquipableCount = 4,
-                                GetTime = DateTimeOffset.UtcNow,
-                                IsFavorite = false,
-                                IsNew = false,
-                            }
-                    )
-                )
-                .Concat(
-                    DefaultSavefileData.ThreeStarCrests.Select(
-                        x =>
-                            new DbAbilityCrest()
-                            {
-                                AbilityCrestId = x,
-                                BuildupCount = 10,
-                                LimitBreakCount = 4,
-                                DeviceAccountId = deviceAccountId,
-                                AttackPlusCount = 50,
-                                HpPlusCount = 50,
-                                EquipableCount = 4,
-                                GetTime = DateTimeOffset.UtcNow,
-                                IsFavorite = false,
-                                IsNew = false,
-                            }
-                    )
-                )
-                .Concat(
-                    DefaultSavefileData.SinDomCrests.Select(
-                        x =>
-                            new DbAbilityCrest()
-                            {
-                                AbilityCrestId = x,
-                                BuildupCount = 30,
-                                LimitBreakCount = 4,
-                                DeviceAccountId = deviceAccountId,
-                                AttackPlusCount = 40,
-                                HpPlusCount = 40,
-                                EquipableCount = 4,
-                                GetTime = DateTimeOffset.UtcNow,
-                                IsFavorite = false,
-                                IsNew = false,
-                            }
-                    )
-                )
-        );
-    }
-
-    private async Task AddDefaultDragons(string deviceAccountId)
-    {
-        await this.apiContext.PlayerDragonData.AddRangeAsync(
-            Enumerable
-                .Repeat(
-                    DefaultSavefileData.Dragons.Select(
-                        x =>
-                            new DbPlayerDragonData()
-                            {
-                                DeviceAccountId = deviceAccountId,
-                                DragonId = x,
-                                Level = 100,
-                                LimitBreakCount = 4,
-                                Ability1Level = 5,
-                                Ability2Level = 5,
-                                Skill1Level = 2,
-                                AttackPlusCount = 50,
-                                HpPlusCount = 50,
-                                Exp = 1_240_020,
-                                GetTime = DateTimeOffset.UtcNow,
-                                IsLock = false,
-                                IsNew = false,
-                            }
-                    ),
-                    DefaultSavefileData.FreeDragonCount
-                )
-                .SelectMany(x => x)
-        );
-
-        await this.apiContext.PlayerDragonReliability.AddRangeAsync(
-            DefaultSavefileData.Dragons.Select(
-                x => DbPlayerDragonReliabilityFactory.Create(deviceAccountId, x)
-            )
-        );
-    }
-
-    private async Task AddDefaultWeapons(string deviceAccountId)
-    {
-        await this.apiContext.PlayerWeapons.AddRangeAsync(
-            DefaultSavefileData.Weapons.Select(
-                x =>
-                    new DbWeaponBody()
-                    {
-                        DeviceAccountId = deviceAccountId,
-                        WeaponBodyId = x,
-                        BuildupCount = 80,
-                        LimitBreakCount = 8,
-                        LimitOverCount = 1,
-                        EquipableCount = 4,
-                        AdditionalCrestSlotType1Count = 1,
-                        AdditionalCrestSlotType2Count = 0,
-                        AdditionalCrestSlotType3Count = 2,
-                        FortPassiveCharaWeaponBuildupCount = 1,
-                        IsNew = true,
-                        GetTime = DateTimeOffset.UtcNow,
-                    }
-            )
-        );
-    }
-
-    private async Task AddDefaultMaterials(string deviceAccountId, int defaultQuantity = 10000)
-    {
-        await this.apiContext.PlayerMaterials.AddRangeAsync(
-            DefaultSavefileData.UpgradeMaterials.Select(
-                x =>
-                    new DbPlayerMaterial()
-                    {
-                        DeviceAccountId = deviceAccountId,
-                        MaterialId = x,
-                        Quantity = defaultQuantity
-                    }
-            )
-        );
-    }
-
     internal static class DefaultSavefileData
     {
-        public static readonly IReadOnlyList<Charas> Characters = new List<Charas>()
-        {
-            Charas.ThePrince
-        };
+        public static readonly ImmutableList<Charas> Characters = MasterAsset.CharaData.Enumerable
+            .Where(x => x.Rarity == 3 && x.IsPlayable)
+            .Select(x => x.Id)
+            .Append(Charas.ThePrince)
+            .ToImmutableList();
 
         public const int PartySlotCount = 54;
-
-        public static readonly IReadOnlyList<AbilityCrests> FiveStarCrests =
-            new List<AbilityCrests>()
-            {
-                // Generic SD
-                AbilityCrests.ValiantCrown,
-                AbilityCrests.HeraldsofHinomoto,
-                // Generic strength
-                AbilityCrests.PecorinesGrandAdventure,
-                AbilityCrests.PrimalCrisis,
-                AbilityCrests.MemoryofaFriend,
-                // FS
-                AbilityCrests.HereCometheSealers,
-                // Generic crit
-                AbilityCrests.ThirdAnniversary,
-                AbilityCrests.LevinsChampion,
-                // Punishers
-                AbilityCrests.MeandMyBestie, // Burn
-                AbilityCrests.IntheLimelight, // Scorchrend
-                AbilityCrests.WingsofRebellionatRest, // Frostbite
-                AbilityCrests.AManUnchanging, // Poison
-                AbilityCrests.SweetSurprise, // Scorchrend
-                AbilityCrests.SpiritoftheSeason, // Paralysis
-                AbilityCrests.ExtremeTeamwork, // Flashburn
-                AbilityCrests.WelcometotheOpera, // Shadowblight
-                // Support
-                AbilityCrests.JewelsoftheSun,
-                AbilityCrests.StudyRabbits,
-                AbilityCrests.GiveMeYourWounded,
-                AbilityCrests.ProperMaintenance,
-                AbilityCrests.CastleCheerCorps,
-                // Misc
-                AbilityCrests.TheChocolatiers,
-                AbilityCrests.WorthyRivals,
-                AbilityCrests.AnAncientOath,
-            };
-
-        public static readonly IReadOnlyList<AbilityCrests> FourStarCrests =
-            new List<AbilityCrests>()
-            {
-                // Punishers
-                AbilityCrests.ThePlaguebringer,
-                AbilityCrests.HisCleverBrother,
-                AbilityCrests.AButlersSmile,
-                AbilityCrests.TheNoblesDayOff,
-                // Misc
-                AbilityCrests.FromWhenceHeComes,
-                AbilityCrests.SnipersAllure,
-                AbilityCrests.LunarFestivities,
-                AbilityCrests.BeautifulNothingness,
-            };
-
-        public static readonly IReadOnlyList<AbilityCrests> ThreeStarCrests =
-            new List<AbilityCrests>()
-            {
-                AbilityCrests.Bellathorna,
-                // Technically Dragon Arcanum is 2 star but whatever
-                AbilityCrests.DragonArcanum,
-                AbilityCrests.DragonsNest
-            };
-
-        public static readonly IReadOnlyList<AbilityCrests> SinDomCrests = new List<AbilityCrests>()
-        {
-            // SD
-            AbilityCrests.TutelarysDestinyWolfsBoon,
-            AbilityCrests.AppleliciousDreamsButterflysBoon,
-            AbilityCrests.AnUnfreezingFlowerDeersBoon,
-            AbilityCrests.AKnightsDreamAxesBoon,
-            // Psalm
-            AbilityCrests.PromisedPietyStaffsBoon,
-            AbilityCrests.RavenousFireCrownsBoon,
-            AbilityCrests.MaskofDeterminationBowsBoon
-        };
-
-        public const int FreeDragonCount = 4;
-
-        public static readonly IReadOnlyList<Dragons> Dragons = new List<Dragons>()
-        {
-            Shared.Definitions.Enums.Dragons.GalaRebornAgni,
-            Shared.Definitions.Enums.Dragons.Horus,
-            Shared.Definitions.Enums.Dragons.GalaRebornPoseidon,
-            Shared.Definitions.Enums.Dragons.GaibhneCreidhne,
-            Shared.Definitions.Enums.Dragons.GalaRebornZephyr,
-            Shared.Definitions.Enums.Dragons.Freyja,
-            Shared.Definitions.Enums.Dragons.GalaRebornJeanne,
-            Shared.Definitions.Enums.Dragons.TieShanGongzhu,
-            Shared.Definitions.Enums.Dragons.GalaRebornNidhogg,
-            Shared.Definitions.Enums.Dragons.Azazel
-        };
-
-        public static readonly IReadOnlyList<WeaponBodies> Weapons = new List<WeaponBodies>()
-        {
-            // Flame
-            WeaponBodies.PrimalCrimson,
-            WeaponBodies.RagingConflagration,
-            WeaponBodies.FlamerulersFang,
-            WeaponBodies.NobleCrimsonHeat,
-            WeaponBodies.OmniflameLance,
-            WeaponBodies.ValkyriesHellfire,
-            WeaponBodies.Hellblaze,
-            WeaponBodies.Flamerollick,
-            WeaponBodies.BigBangTrigger,
-            // Water
-            WeaponBodies.PrimalAqua,
-            WeaponBodies.CalamitousTorrent,
-            WeaponBodies.TiderulersFang,
-            WeaponBodies.LimpidCascade,
-            WeaponBodies.SapphireMercurius,
-            WeaponBodies.AqueousPrison,
-            WeaponBodies.RuleroftheJeweledTide,
-            WeaponBodies.AquamarineTrigger,
-            // Wind
-            WeaponBodies.PrimalTempest,
-            WeaponBodies.NobleHorizon,
-            WeaponBodies.WindrulersFang,
-            WeaponBodies.TempestsGuide,
-            WeaponBodies.GalesAid,
-            WeaponBodies.JormungandsWrath,
-            WeaponBodies.StormChaser,
-            WeaponBodies.Squallruler,
-            WeaponBodies.CycloneTrigger,
-            // Light
-            WeaponBodies.PrimalLightning,
-            WeaponBodies.DauntingFlash,
-            WeaponBodies.FulminatorsFang,
-            WeaponBodies.IndomitableThundercrash,
-            WeaponBodies.RadiantLightflash,
-            WeaponBodies.JupitersShimmer,
-            WeaponBodies.ElectronBurst,
-            WeaponBodies.CosmicRuler,
-            WeaponBodies.DivineTrigger,
-            // Shadow
-            WeaponBodies.PrimalHex,
-            WeaponBodies.EternalAbyss,
-            WeaponBodies.ShaderulersFang,
-            WeaponBodies.NightfallsDarkbiteAxe,
-            WeaponBodies.EbonPlagueLance,
-            WeaponBodies.NightmareProphecy,
-            WeaponBodies.UmbralChaser,
-            WeaponBodies.ConsumingDarkness,
-            WeaponBodies.DuskTrigger
-        };
-
-        public static readonly IReadOnlyList<Materials> UpgradeMaterials = new List<Materials>()
-        {
-            Materials.GoldCrystal,
-            Materials.SilverCrystal,
-            Materials.BronzeCrystal,
-            Materials.LookingGlass
-        };
     }
-    #endregion
 }
 
 file static class Extensions
