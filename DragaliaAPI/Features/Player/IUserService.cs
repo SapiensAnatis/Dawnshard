d58ba71e6cd233ba0350e296a390aa5e39325292
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2023-07-23 04:25:42 +0200

    Add player level logic

---
+++ DragaliaAPI/Features/Player/IUserService.cs
@@ -0,0 +1,11 @@
+﻿namespace DragaliaAPI.Features.Player;
+
+public interface IUserService
+{
+    int QuestSkipPointMax { get; }
+    int StaminaMultiMax { get; }
+
+    Task AddStamina(StaminaType type, int amount);
+    Task AddQuestSkipPoint(int amount);
+    Task<PlayerLevelResult> AddExperience(int experience);
+}
