fd0ab4ecf69c0661c9c5739ecad9babbe06ed6e6
Jay Malhotra <jay0@me.com>
2023-07-27 22:50:51 +0100

    Attempt to handle ID token refresh correctly

--- DragaliaAPI/Middleware/ExceptionHandlerMiddleware.cs
+++ DragaliaAPI/Middleware/ExceptionHandlerMiddleware.cs
@@ -1,58 +1,76 @@
-﻿using System.Collections.Immutable;
-using System.Diagnostics;
-using System.Net;
+﻿using System.Net;
 using DragaliaAPI.Controllers;
+using DragaliaAPI.Extensions;
 using DragaliaAPI.MessagePack;
 using DragaliaAPI.Models;
 using DragaliaAPI.Services.Exceptions;
 using MessagePack;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Primitives;
 using Microsoft.IdentityModel.Tokens;
 
 namespace DragaliaAPI.Middleware;
 
 public class ExceptionHandlerMiddleware
 {
-    private const ResultCode ServerErrorCode = ResultCode.CommonServerError;
-
-    private const string RefreshIdToken = "Is-Required-Refresh-Id-Token";
-    private const string True = "true";
-
     private readonly RequestDelegate next;
-    private readonly ILogger logger;
+    private static readonly DistributedCacheEntryOptions CacheOptions =
+        new() { AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5) };
 
-    public ExceptionHandlerMiddleware(RequestDelegate next, ILoggerFactory logger)
+    public ExceptionHandlerMiddleware(RequestDelegate next)
     {
         this.next = next;
-        this.logger = logger.CreateLogger<ExceptionHandlerMiddleware>();
     }
 
-    public async Task InvokeAsync(HttpContext context)
+    public async Task InvokeAsync(
+        HttpContext context,
+        ILogger<ExceptionHandlerMiddleware> logger,
+        IDistributedCache cache
+    )
     {
+        Endpoint? endpoint = context.GetEndpoint();
+        bool serializeException =
+            endpoint?.Metadata.GetMetadata<SerializeExceptionAttribute>() is not null;
+
+        string? deviceId = null;
+        if (context.Request.Headers.TryGetValue("DeviceId", out StringValues values))
+            deviceId = values.FirstOrDefault();
+
         try
         {
             await this.next(context);
         }
-        catch (Exception ex)
+        catch (SecurityTokenExpiredException ex) when (serializeException && deviceId is not null)
         {
-            Endpoint? endpoint = context.GetEndpoint();
-            if (endpoint?.Metadata.GetMetadata<SerializeExceptionAttribute>() == null)
-                throw;
+            logger.LogDebug("ID token was expired. Expiry: {expiry}", ex.Expires);
 
+            // This should cause the client to route back to Nintendo login.
+            // However, sometimes the client returns with the same invalid ID token.
+
+            string redisKey = $"refresh_sent:{deviceId}";
+
+            if (await cache.GetStringAsync(redisKey) is not null)
+            {
+                logger.LogWarning("Detected repeated SecurityTokenExpiredException.");
+                throw new DragaliaException(
+                    ResultCode.CommonAuthError,
+                    "Detected repeated SecurityTokenExpiredException."
+                );
+            }
+
+            logger.LogDebug("Issuing ID token refresh request.");
+            await cache.SetStringAsync(redisKey, "true", CacheOptions);
+
+            context.Response.StatusCode = 400;
+            context.Response.Headers.Add("Is-Required-Refresh-Id-Token", "true");
+            return;
+        }
+        catch (Exception ex) when (serializeException)
+        {
             if (context.RequestAborted.IsCancellationRequested)
             {
-                this.logger.LogWarning(ex, "Client cancelled request.");
+                logger.LogWarning(ex, "Client cancelled request.");
                 context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-
-                return;
-            }
-            else if (ex is SessionException)
-            {
-                // Will send back to login
-                this.logger.LogInformation(
-                    "Returning session refresh request due to SessionException"
-                );
-                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-                context.Response.Headers.Add(RefreshIdToken, True);
 
                 return;
             }
@@ -60,15 +78,11 @@ public class ExceptionHandlerMiddleware
             context.Response.ContentType = CustomMessagePackOutputFormatter.ContentType;
             context.Response.StatusCode = 200;
 
-            ResultCode code = ex switch
-            {
-                DragaliaException d => d.Code,
-                SecurityTokenExpiredException => ResultCode.IdTokenError,
-                NotImplementedException => ResultCode.CommonTimeout,
-                _ => ResultCode.CommonServerError
-            };
+            ResultCode code = ex is DragaliaException dragaliaException
+                ? dragaliaException.Code
+                : ResultCode.CommonServerError;
 
-            this.logger.LogError(
+            logger.LogError(
                 ex,
                 "Encountered unhandled exception. Returning result_code {code}",
                 code
@@ -80,5 +94,10 @@ public class ExceptionHandlerMiddleware
                 MessagePackSerializer.Serialize(gameResponse, CustomResolver.Options)
             );
         }
+    }
+
+    private static string? GetRedisKey(string? deviceId)
+    {
+        return deviceId is null ? null : $"refresh_sent:{deviceId}";
     }
 }
