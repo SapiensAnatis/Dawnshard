0344029c9679fb9b68809399b30d5d6cd0702b68
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2023-08-03 19:27:26 +0100

    Fix characters being given without stories (#350)
    Closes #358

--- DragaliaAPI.Database.Test/DbTestFixture.cs
+++ DragaliaAPI.Database.Test/DbTestFixture.cs
@@ -28,9 +28,11 @@ public class DbTestFixture : IDisposable
             .Options;
 
         this.ApiContext = new ApiContext(options);
-        Mock<ILogger<SavefileService>> mockLogger = new(MockBehavior.Loose);
         // Unused for creating saves
+        Mock<ILogger<SavefileService>> mockLogger = new(MockBehavior.Loose);
         Mock<IDistributedCache> mockCache = new(MockBehavior.Loose);
+        // Used but we probably don't want it to actually add characters?
+        Mock<IUnitRepository> mockUnitRepository = new(MockBehavior.Loose);
 
         SavefileService savefileService =
             new(
@@ -41,7 +43,8 @@ public class DbTestFixture : IDisposable
                 ).CreateMapper(),
                 mockLogger.Object,
                 IdentityTestUtils.MockPlayerDetailsService.Object,
-                Enumerable.Empty<ISavefileUpdate>()
+                Enumerable.Empty<ISavefileUpdate>(),
+                mockUnitRepository.Object
             );
         savefileService.Create().Wait();
     }
