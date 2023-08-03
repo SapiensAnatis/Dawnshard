44acd5e39b25dcc9340a3ac63b2380ded924787c
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2023-08-03 20:47:37 +0200

    Add savefile update to add default emblem

---
+++ DragaliaAPI/Features/SavefileUpdate/V10Update.cs
@@ -0,0 +1,15 @@
+﻿using DragaliaAPI.Features.Emblem;
+using DragaliaAPI.Shared.Definitions.Enums;
+
+namespace DragaliaAPI.Features.SavefileUpdate;
+
+public class V10Update(IEmblemRepository emblemRepository) : ISavefileUpdate
+{
+    public int SavefileVersion => 10;
+
+    public async Task Apply()
+    {
+        if (!await emblemRepository.HasEmblem(Emblems.DragonbloodPrince))
+            emblemRepository.AddEmblem(Emblems.DragonbloodPrince);
+    }
+}
