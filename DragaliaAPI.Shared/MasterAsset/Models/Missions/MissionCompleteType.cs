508c5995e82d246ca19052435a07b7def64e3e87
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2023-08-06 19:23:04 +0200

    Rework MissionProgressionInfo

---
+++ DragaliaAPI.Shared/MasterAsset/Models/Missions/MissionCompleteType.cs
@@ -0,0 +1,32 @@
+﻿namespace DragaliaAPI.Shared.MasterAsset.Models.Missions;
+
+public enum MissionCompleteType
+{
+    // Fort
+    FortPlantLevelUp,
+    FortPlantBuilt,
+    FortPlantPlaced,
+    FortLevelUp,
+
+    // Quest
+    QuestCleared,
+    QuestGroupCleared,
+
+    // Story
+    QuestStoryCleared,
+
+    // Unit
+    WeaponEarned,
+    WeaponRefined,
+
+    AbilityCrestBuildupPlusCount,
+    AbilityCrestLevelUp,
+
+    CharacterBuildupPlusCount,
+    CharacterLevelUp,
+
+    // Misc
+    ItemSummon,
+    PlayerLevelUp,
+    AccountLinked
+}
