6a65d912d4dd20140e4a44849f1d456bc6f4b6c5
Jay Malhotra <jay0@me.com>
2023-08-13 15:07:02 +0100

    Add Mym helper

--- DragaliaAPI/Services/Game/HelperService.cs
+++ DragaliaAPI/Services/Game/HelperService.cs
@@ -2294,6 +2294,139 @@ public class HelperService : IHelperService
                             }
                         },
                         guild = new() { guild_id = 0, guild_name = "Guild" }
+                    },
+                    new()
+                    {
+                        viewer_id = 1017,
+                        name = "Shiny ☆",
+                        level = 250,
+                        last_login_date = DateTimeOffset.UtcNow - TimeSpan.FromDays(1),
+                        emblem_id = (Emblems)10350303,
+                        max_party_power = 9999,
+                        support_chara = new()
+                        {
+                            chara_id = Charas.GalaMym,
+                            level = 100,
+                            additional_max_level = 20,
+                            rarity = 5,
+                            hp = 898,
+                            attack = 612,
+                            hp_plus_count = 100,
+                            attack_plus_count = 100,
+                            ability_1_level = 3,
+                            ability_2_level = 3,
+                            ability_3_level = 3,
+                            ex_ability_level = 5,
+                            ex_ability_2_level = 5,
+                            skill_1_level = 4,
+                            skill_2_level = 3,
+                            is_unlock_edit_skill = true,
+                        },
+                        support_dragon = new()
+                        {
+                            dragon_key_id = 0,
+                            dragon_id = Dragons.Horus,
+                            level = 100,
+                            hp = 368,
+                            attack = 128,
+                            skill_1_level = 2,
+                            ability_1_level = 5,
+                            ability_2_level = 5,
+                            hp_plus_count = 50,
+                            attack_plus_count = 50,
+                            limit_break_count = 4
+                        },
+                        support_weapon_body = new()
+                        {
+                            weapon_body_id = WeaponBodies.OmniflameLance,
+                            buildup_count = 80,
+                            limit_break_count = 8,
+                            limit_over_count = 1,
+                            equipable_count = 4,
+                            additional_crest_slot_type_1_count = 1,
+                            additional_crest_slot_type_2_count = 0,
+                            additional_crest_slot_type_3_count = 2
+                        },
+                        support_talisman = new()
+                        {
+                            talisman_key_id = 0,
+                            talisman_id = Talismans.GalaMym,
+                            talisman_ability_id_1 = 340000030,
+                            talisman_ability_id_2 = 340000132
+                        },
+                        support_crest_slot_type_1_list = new List<AtgenSupportCrestSlotType1List>()
+                        {
+                            new()
+                            {
+                                ability_crest_id = AbilityCrests.MeandMyBestie,
+                                buildup_count = 50,
+                                limit_break_count = 4,
+                                hp_plus_count = 50,
+                                attack_plus_count = 50,
+                                equipable_count = 4
+                            },
+                            new()
+                            {
+                                ability_crest_id = AbilityCrests.TheCutieCompetition,
+                                buildup_count = 50,
+                                limit_break_count = 4,
+                                hp_plus_count = 50,
+                                attack_plus_count = 50,
+                                equipable_count = 4
+                            },
+                            new()
+                            {
+                                ability_crest_id = AbilityCrests.WorthyRivals,
+                                buildup_count = 50,
+                                limit_break_count = 4,
+                                hp_plus_count = 50,
+                                attack_plus_count = 50,
+                                equipable_count = 4
+                            },
+                        },
+                        support_crest_slot_type_2_list = new List<AtgenSupportCrestSlotType1List>()
+                        {
+                            new()
+                            {
+                                ability_crest_id = AbilityCrests.ChariotDrift,
+                                buildup_count = 40,
+                                limit_break_count = 4,
+                                hp_plus_count = 50,
+                                attack_plus_count = 50,
+                                equipable_count = 4
+                            },
+                            new()
+                            {
+                                ability_crest_id = AbilityCrests.DragonsNest,
+                                buildup_count = 20,
+                                limit_break_count = 4,
+                                hp_plus_count = 50,
+                                attack_plus_count = 50,
+                                equipable_count = 4
+                            },
+                        },
+                        support_crest_slot_type_3_list = new List<AtgenSupportCrestSlotType1List>()
+                        {
+                            new()
+                            {
+                                ability_crest_id = AbilityCrests.TutelarysDestinyWolfsBoon,
+                                buildup_count = 30,
+                                limit_break_count = 4,
+                                hp_plus_count = 40,
+                                attack_plus_count = 40,
+                                equipable_count = 4
+                            },
+                            new()
+                            {
+                                ability_crest_id = AbilityCrests.CrownofLightSerpentsBoon,
+                                buildup_count = 30,
+                                limit_break_count = 4,
+                                hp_plus_count = 40,
+                                attack_plus_count = 40,
+                                equipable_count = 4
+                            }
+                        },
+                        guild = new() { guild_id = 0, guild_name = "Guild" }
                     }
                 },
                 support_user_detail_list = new List<AtgenSupportUserDetailList>()
@@ -2397,6 +2530,12 @@ public class HelperService : IHelperService
                     new()
                     {
                         viewer_id = 1016,
+                        gettable_mana_point = 50,
+                        is_friend = true
+                    },
+                    new()
+                    {
+                        viewer_id = 1017,
                         gettable_mana_point = 50,
                         is_friend = true
                     },
