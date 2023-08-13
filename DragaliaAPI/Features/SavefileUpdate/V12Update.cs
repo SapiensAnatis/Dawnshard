749abec32be72c807aeb4719b50cb3b3a6386211
sockperson <supertestosterone@gmail.com>
2023-08-12 18:00:55 -0700

    forgot to add the actual file lol

---
+++ DragaliaAPI/Features/SavefileUpdate/V12Update.cs
@@ -0,0 +1,22 @@
+﻿using DragaliaAPI.Database.Repositories;
+using DragaliaAPI.Features.Wall;
+using DragaliaAPI.Services.Game;
+
+namespace DragaliaAPI.Features.SavefileUpdate;
+
+public class V12Update(
+    IWallRepository wallRepository,
+    IWallService wallService,
+    IStoryRepository storyRepository
+) : ISavefileUpdate
+{
+    public int SavefileVersion => 12;
+
+    public async Task Apply()
+    {
+        if (await storyRepository.HasReadQuestStory(TutorialService.TutorialStoryIds.MercurialGauntlet))
+        {
+            await wallRepository.InitializeWall();
+        }
+    }
+}
