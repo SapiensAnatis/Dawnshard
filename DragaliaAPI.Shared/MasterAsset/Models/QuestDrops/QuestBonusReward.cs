2fa1e51d48f278ee8daa081632028fbd28b91530
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2023-08-15 22:49:58 +0100

    Add rewards to quest bonuses (#394)

---
+++ DragaliaAPI.Shared/MasterAsset/Models/QuestDrops/QuestBonusReward.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+using DragaliaAPI.Shared.Definitions.Enums;
+
+namespace DragaliaAPI.Shared.MasterAsset.Models.QuestDrops;
+
+public record QuestBonusReward(int QuestId, IEnumerable<QuestBonusDrop> Bonuses);
+
+// TODO: Extend with random quantity variation
+public record QuestBonusDrop(
+    [property: JsonConverter(typeof(JsonStringEnumConverter))] EntityTypes EntityType,
+    int Id,
+    int Quantity
+);
