80ffc35e7a844e4f833923f78f9a3775f76e655b
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2023-08-28 17:01:05 +0100

    Check for AccountId claim before adding to log context

--- DragaliaAPI/Middleware/PlayerIdentityLoggingMiddleware.cs
+++ DragaliaAPI/Middleware/PlayerIdentityLoggingMiddleware.cs
@@ -8,7 +8,10 @@ public class PlayerIdentityLoggingMiddleware(IPlayerIdentityService playerIdenti
 {
     public Task InvokeAsync(HttpContext context, RequestDelegate next)
     {
-        if (context.User.Identity?.IsAuthenticated ?? false)
+        if (
+            (context.User.Identity?.IsAuthenticated ?? false)
+            && context.User.HasClaim(x => x.Type == CustomClaimType.AccountId)
+        )
         {
             LogContext.PushProperty("AccountId", playerIdentityService.AccountId);
             LogContext.PushProperty("ViewerId", playerIdentityService.ViewerId);
