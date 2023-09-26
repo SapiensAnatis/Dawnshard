e4dc44ee435d1eb8d1ab9e7adf8a72538b2ca668
dstamatis <Dionyssioss@gmail.com>
2023-09-27 00:06:46 +0200

    New Version

--- DragaliaAPI/Controllers/Dragalia/VersionController.cs
+++ DragaliaAPI/Controllers/Dragalia/VersionController.cs
@@ -9,7 +9,7 @@ namespace DragaliaAPI.Controllers.Dragalia;
 public class VersionController : DragaliaControllerBase
 {
     private const string AndroidResourceVersion = "y2XM6giU6zz56wCm";
-    private const string IosResourceVersion = "BxFd4oT2KnO!eJlu";
+    private const string IosResourceVersion = "h36dI3Buk9ltomn5";
 
     [HttpPost]
     [Route("get_resource_version")]
