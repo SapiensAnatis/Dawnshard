49f8ec63a1afd1f46fea6b3876a3bb84d01ec5d1
Nathaniel Church <38626341+FatesTimelines1337@users.noreply.github.com>
2023-10-23 11:32:13 -0400

    Update QuestTreasureData.cs

--- DragaliaAPI.Shared/MasterAsset/Models/QuestTreasureData.cs
+++ DragaliaAPI.Shared/MasterAsset/Models/QuestTreasureData.cs
@@ -3,5 +3,5 @@ public record QuestTreasureData(
     int AddMaxDragonStorage,
     int EntityType, // 4 - Rupies, 18 - Mana, 8 - Material, 17 - Summon Ticket, 2 - Item (Honey)
     int EntityId,
-    int EntityQuantity,
+    int EntityQuantity
 );
