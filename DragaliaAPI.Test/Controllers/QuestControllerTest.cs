a6de39cb21485cb5cb9ccbb75d0e720aef865b1c
Nathaniel Church <38626341+FatesTimelines1337@users.noreply.github.com>
2023-10-25 12:34:18 -0400

    Add missing mock values and api context

--- DragaliaAPI.Test/Controllers/QuestControllerTest.cs
+++ DragaliaAPI.Test/Controllers/QuestControllerTest.cs
@@ -1,4 +1,7 @@
 using DragaliaAPI.Controllers.Dragalia;
+using DragaliaAPI.Database;
+using DragaliaAPI.Database.Repositories;
+using DragaliaAPI.Shared.PlayerDetails;
 using DragaliaAPI.Features.ClearParty;
 using DragaliaAPI.Features.Dungeon;
 using DragaliaAPI.Features.Quest;
@@ -16,6 +19,9 @@ public class QuestControllerTest
     private readonly Mock<IQuestDropService> mockQuestRewardService;
     private readonly Mock<IUpdateDataService> mockUpdateDataService;
     private readonly Mock<IClearPartyService> mockClearPartyService;
+    private readonly Mock<IRewardService> mockRewardService;
+    private readonly Mock<IUserDataRepository> mockUserDataRepository;
+    private readonly Mock<IPlayerIdentityService> mockPlayerIdentityService;
     private readonly Mock<ILogger<QuestController>> mockLogger;
 
     private readonly QuestController questController;
@@ -27,6 +33,9 @@ public class QuestControllerTest
         this.mockQuestRewardService = new(MockBehavior.Strict);
         this.mockUpdateDataService = new(MockBehavior.Strict);
         this.mockClearPartyService = new(MockBehavior.Strict);
+        this.mockRewardService = new(MockBehavior.Strict);
+        this.mockUserDataRepository = new(MockBehavior.Strict);
+        this.mockPlayerIdentityService = new(MockBehavior.Strict);
         this.mockLogger = new(MockBehavior.Loose);
 
         this.questController = new(
@@ -35,6 +44,10 @@ public class QuestControllerTest
             this.mockQuestRewardService.Object,
             this.mockUpdateDataService.Object,
             this.mockClearPartyService.Object,
+            this.mockRewardService.Object,
+            this.mockUserDataRepository.Object,
+            this.mockPlayerIdentityService.Object,
+            this.ApiContext,
             this.mockLogger.Object
         );
     }
