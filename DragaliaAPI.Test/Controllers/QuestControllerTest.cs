0b700c7b4e26a2dbfc31dfa251fde702e8ce38dd
Nathaniel Church <38626341+FatesTimelines1337@users.noreply.github.com>
2023-10-26 13:15:54 -0400

    revert test back to base coding

--- DragaliaAPI.Test/Controllers/QuestControllerTest.cs
+++ DragaliaAPI.Test/Controllers/QuestControllerTest.cs
@@ -1,19 +1,11 @@
 using DragaliaAPI.Controllers.Dragalia;
-using DragaliaAPI.Database;
-using DragaliaAPI.Database.Repositories;
-using DragaliaAPI.Database.Entities;
-using DragaliaAPI.Shared.PlayerDetails;
-using DragaliaAPI.Shared.Definitions.Enums;
 using DragaliaAPI.Features.ClearParty;
 using DragaliaAPI.Features.Dungeon;
 using DragaliaAPI.Features.Quest;
-using DragaliaAPI.Features.Reward;
 using DragaliaAPI.Models.Generated;
-using DragaliaAPI.Test.Utils;
 using DragaliaAPI.Services;
+using DragaliaAPI.Shared.Definitions.Enums;
 using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace DragaliaAPI.Test.Controllers;
 
@@ -24,12 +16,8 @@ public class QuestControllerTest
     private readonly Mock<IQuestDropService> mockQuestRewardService;
     private readonly Mock<IUpdateDataService> mockUpdateDataService;
     private readonly Mock<IClearPartyService> mockClearPartyService;
-    private readonly Mock<IRewardService> mockRewardService;
-    private readonly Mock<IUserDataRepository> mockUserDataRepository;
-    private readonly Mock<IPlayerIdentityService> mockPlayerIdentityService;
     private readonly Mock<ILogger<QuestController>> mockLogger;
-    private readonly ApiContext apiContext;
-    private readonly string DbName = $"DbTestFixture-{Guid.NewGuid()}";
+
     private readonly QuestController questController;
 
     public QuestControllerTest()
@@ -39,17 +27,6 @@ public class QuestControllerTest
         this.mockQuestRewardService = new(MockBehavior.Strict);
         this.mockUpdateDataService = new(MockBehavior.Strict);
         this.mockClearPartyService = new(MockBehavior.Strict);
-        this.mockRewardService = new(MockBehavior.Strict);
-        this.mockUserDataRepository = new(MockBehavior.Strict);
-        this.mockPlayerIdentityService = new(MockBehavior.Strict);
-        //this.mockPlayerIdentityService.SetupGet(x => x.AccountId).Returns(DeviceAccountId);
-        DbContextOptions<ApiContext> options = new DbContextOptionsBuilder<ApiContext>()
-            .UseInMemoryDatabase(DbName)
-            .EnableSensitiveDataLogging()
-            .ConfigureWarnings(config => config.Ignore(InMemoryEventId.TransactionIgnoredWarning))
-            .Options;
-
-        this.apiContext = new ApiContext(options);
         this.mockLogger = new(MockBehavior.Loose);
 
         this.questController = new(
@@ -58,10 +35,6 @@ public class QuestControllerTest
             this.mockQuestRewardService.Object,
             this.mockUpdateDataService.Object,
             this.mockClearPartyService.Object,
-            this.mockRewardService.Object,
-            this.mockUserDataRepository.Object,
-            this.mockPlayerIdentityService.Object,
-            this.apiContext,
             this.mockLogger.Object
         );
     }
@@ -138,16 +111,5 @@ public class QuestControllerTest
 
         this.mockStoryService.VerifyAll();
         this.mockUpdateDataService.VerifyAll();
-    }
-
-    [Fact]
-    public async Task OpenTreasure_RewardsCorrect()
-    {
-        //this.mockPlayerIdentityService.SetupGet(x => x.AccountId).Returns("id");
-
-
-        this.mockRewardService.VerifyAll();
-        this.mockUserDataRepository.VerifyAll();
-        //this.mockPlayerIdentityService.VerifyAll();
     }
 }
