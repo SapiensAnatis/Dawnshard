4a1343c5ad48861e24f5702b5b050730537dfc1b
Nathaniel Church <38626341+FatesTimelines1337@users.noreply.github.com>
2023-10-26 13:36:11 -0400

    Readd missing controller values

--- DragaliaAPI.Test/Controllers/QuestControllerTest.cs
+++ DragaliaAPI.Test/Controllers/QuestControllerTest.cs
@@ -1,22 +1,31 @@
 using DragaliaAPI.Controllers.Dragalia;
+using DragaliaAPI.Database;
+using DragaliaAPI.Database.Repositories;
+using DragaliaAPI.Shared.PlayerDetails;
 using DragaliaAPI.Features.ClearParty;
 using DragaliaAPI.Features.Dungeon;
 using DragaliaAPI.Features.Quest;
+using DragaliaAPI.Features.Reward;
 using DragaliaAPI.Models.Generated;
 using DragaliaAPI.Services;
 using DragaliaAPI.Shared.Definitions.Enums;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 namespace DragaliaAPI.Test.Controllers;
 
 public class QuestControllerTest
 {
-    private readonly Mock<IStoryService> mockStoryService;
-    private readonly Mock<IHelperService> mockHelperService;
     private readonly Mock<IQuestDropService> mockQuestRewardService;
     private readonly Mock<IUpdateDataService> mockUpdateDataService;
     private readonly Mock<IClearPartyService> mockClearPartyService;
+    private readonly Mock<IRewardService> mockRewardService;
+    private readonly Mock<IUserDataRepository> mockUserDataRepository;
+    private readonly Mock<IPlayerIdentityService> mockPlayerIdentityService;
     private readonly Mock<ILogger<QuestController>> mockLogger;
+    private readonly ApiContext apiContext;
+    private readonly string DbName = $"DbTestFixture-{Guid.NewGuid()}";
 
     private readonly QuestController questController;
 
@@ -27,6 +36,16 @@ public class QuestControllerTest
         this.mockQuestRewardService = new(MockBehavior.Strict);
         this.mockUpdateDataService = new(MockBehavior.Strict);
         this.mockClearPartyService = new(MockBehavior.Strict);
+        this.mockRewardService = new(MockBehavior.Strict);
+        this.mockUserDataRepository = new(MockBehavior.Strict);
+        this.mockPlayerIdentityService = new(MockBehavior.Strict);
+        DbContextOptions<ApiContext> options = new DbContextOptionsBuilder<ApiContext>()
+            .UseInMemoryDatabase(DbName)
+            .EnableSensitiveDataLogging()
+            .ConfigureWarnings(config => config.Ignore(InMemoryEventId.TransactionIgnoredWarning))
+            .Options;
+
+        this.apiContext = new ApiContext(options);
         this.mockLogger = new(MockBehavior.Loose);
 
         this.questController = new(
@@ -35,6 +54,10 @@ public class QuestControllerTest
             this.mockQuestRewardService.Object,
             this.mockUpdateDataService.Object,
             this.mockClearPartyService.Object,
+            this.mockRewardService.Object,
+            this.mockUserDataRepository.Object,
+            this.mockPlayerIdentityService.Object,
+            this.apiContext,
             this.mockLogger.Object
         );
     }
