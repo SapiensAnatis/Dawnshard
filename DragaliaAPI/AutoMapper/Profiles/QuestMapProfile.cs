0bed6feda94ac84cb2003d35953665c43e79c2ad
Nathaniel Church <38626341+FatesTimelines1337@users.noreply.github.com>
2023-11-01 21:57:07 -0400

    Add reverse map to treasure list

--- DragaliaAPI/AutoMapper/Profiles/QuestMapProfile.cs
+++ DragaliaAPI/AutoMapper/Profiles/QuestMapProfile.cs
@@ -13,7 +13,7 @@ public class QuestMapProfile : Profile
 
         this.CreateMap<DbQuestEvent, QuestEventList>().ReverseMap();
 
-        this.CreateMap<DbQuestTreasureList, QuestTreasureList>();
+        this.CreateMap<DbQuestTreasureList, QuestTreasureList>().ReverseMap();
 
         this.CreateMap<DbPlayerStoryState, QuestStoryList>()
             .ForMember(x => x.quest_story_id, o => o.MapFrom(nameof(DbPlayerStoryState.StoryId)));
