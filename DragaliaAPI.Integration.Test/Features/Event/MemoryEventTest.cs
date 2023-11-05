cb337f459222e2278209019f117cecd94c70c9c8
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2023-11-05 18:26:59 +0000

    Add tests

---
+++ DragaliaAPI.Integration.Test/Features/Event/MemoryEventTest.cs
@@ -0,0 +1,24 @@
+namespace DragaliaAPI.Integration.Test.Features.Event;
+
+public class MemoryEventTest : TestFixture
+{
+    public MemoryEventTest(CustomWebApplicationFactory factory, ITestOutputHelper outputHelper)
+        : base(factory, outputHelper) { }
+
+    [Fact]
+    public async Task MemoryEvent_Activate_CompletesParticipationMission()
+    {
+        int eventId = 20845; // Toll of the Deep
+
+        MemoryEventActivateData result = (
+            await this.Client.PostMsgpack<MemoryEventActivateData>(
+                "memory_event/activate",
+                new MemoryEventActivateRequest() { event_id = eventId }
+            )
+        ).data;
+
+        result.update_data_list.mission_notice.memory_event_mission_notice.new_complete_mission_id_list
+            .Should()
+            .Contain(10220101); // Participate in the Event
+    }
+}
--- DragaliaAPI.Test/Features/Dungeon/Record/DungeonRecordServiceTest.cs
+++ DragaliaAPI.Test/Features/Dungeon/Record/DungeonRecordServiceTest.cs
@@ -156,7 +156,7 @@ public class DungeonRecordServiceTest
         int takeBoostAccumulatePoint = 40;
 
         this.mockQuestService
-            .Setup(x => x.ProcessQuestCompletion(lSurtrSoloId, 10f, 1))
+            .Setup(x => x.ProcessQuestCompletion(session, playRecord))
             .ReturnsAsync((mockQuest, true, new List<AtgenFirstClearSet>()));
 
         this.mockUserService
