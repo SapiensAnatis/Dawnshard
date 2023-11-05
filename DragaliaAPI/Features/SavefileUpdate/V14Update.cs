9f8217424bd66914347e7c1066bb37ffe0968552
Olivier Mayer <o.mayerff@gmail.com>
2023-11-05 18:08:41 +0100

    Savefile update: Add V14 update for adventurer story epithets

---
+++ DragaliaAPI/Features/SavefileUpdate/V14Update.cs
@@ -0,0 +1,34 @@
+using DragaliaAPI.Database.Repositories;
+using DragaliaAPI.Features.Reward;
+using DragaliaAPI.Shared.Definitions.Enums;
+using DragaliaAPI.Shared.MasterAsset;
+using Microsoft.EntityFrameworkCore;
+
+namespace DragaliaAPI.Features.SavefileUpdate;
+
+public class V14Update(
+    IStoryRepository storyRepository,
+    IRewardService rewardService
+) : ISavefileUpdate
+{
+    public int SavefileVersion => 14;
+
+    public async Task Apply()
+    {
+        int[] readStoryIdList = await storyRepository.UnitStories
+            .Where(x => (x.State == StoryState.Read) && x.StoryType == StoryTypes.Chara)
+            .Select(x => x.StoryId)
+            .ToArrayAsync();
+        int storyCharacterId;
+        int[] characterStoryList;
+        foreach (int readStoryId in readStoryIdList)
+        {
+            storyCharacterId = MasterAsset.UnitStory[readStoryId].ReleaseTriggerId;
+            characterStoryList = MasterAsset.CharaStories[storyCharacterId].storyIds;
+            if (readStoryId == characterStoryList.Last())
+            {
+                await rewardService.GrantReward(new(EntityTypes.Title, storyCharacterId, 1));
+            }
+        }
+    }
+}
