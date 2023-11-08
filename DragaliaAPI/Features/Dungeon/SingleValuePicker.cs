957d283a056ba040f2b3c132321b72e046a32ee6
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2023-11-08 19:41:06 +0000

    Fix single-enemy quests throwing errors on start (#490)
    FluentRandomPicker throws NotEnoughValuesToPickException when calling
    .WithPrioritizedElements with a collection that has fewer than 2 values.
    This is a problem since many boss quests only have 1 enemy. Some quests
    may only have 1 drop as well.
    
    This commit implements an alternative implementation of IPick<T> that
    handles the single-element case and create a factory method to handle
    constructing either FluentRandomPicker's implementation or the custom
    implementation depending on the array length..

---
+++ DragaliaAPI/Features/Dungeon/SingleValuePicker.cs
@@ -0,0 +1,19 @@
+using FluentRandomPicker.FluentInterfaces.General;
+
+namespace DragaliaAPI.Features.Dungeon.Start;
+
+public class SingleValuePicker<T> : IPick<T>
+{
+    private readonly T singleElement;
+
+    public SingleValuePicker(T singleElement)
+    {
+        this.singleElement = singleElement;
+    }
+
+    public IEnumerable<T> Pick(int n) => Enumerable.Repeat(this.singleElement, n);
+
+    public T PickOne() => this.singleElement;
+
+    public IEnumerable<T> PickDistinct(int n) => throw new NotSupportedException();
+}
