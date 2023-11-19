SapiensAnatis/Dawnshard

commit 572dc5afccc61b789c130b6719f24ef952cc1cb0

Change all primary keys to ViewerId (#500)
Currently all of the tables use DeviceAccountId as a primary key which
has no fixed length and is non-sequential.

Speculatively, this can make joins slower since comparing a string is
likely to be slower than an int, but reading online it seems unlikely
there will be a noticeable performance impact for a DB the size of
Dawnshard's.

It should however improve the organisation of data and make manual
querying / debugging easier

DragaliaAPI.Integration.Test/Other/SavefileImportTest.cs
@@ -50,10 +50,7 @@ public class SavefileImportTest : TestFixture
     public async Task Import_LoadIndexReturnsImportedSavefile()
     {
         string savefileJson = File.ReadAllText(Path.Join("Data", "endgame_savefile.json"));
-        long viewerId = this.ApiContext
-            .PlayerUserData
-            .Single(x => x.DeviceAccountId == DeviceAccountId)
-            .ViewerId;
+        long viewerId = this.ApiContext.PlayerUserData.Single(x => x.ViewerId == ViewerId).ViewerId;
 
         LoadIndexData savefile = JsonSerializer
             .Deserialize<DragaliaResponse<LoadIndexData>>(savefileJson, ApiJsonOptions.Instance)!
@@ -149,24 +146,21 @@ public class SavefileImportTest : TestFixture
     [Fact]
     public async Task Import_PropertiesMappedCorrectly()
     {
-        long viewerId = this.ApiContext
-            .PlayerUserData
-            .Single(x => x.DeviceAccountId == DeviceAccountId)
-            .ViewerId;
+        long viewerId = this.ApiContext.PlayerUserData.Single(x => x.ViewerId == ViewerId).ViewerId;
 
         HttpContent content = PrepareSavefileRequest();
         await this.Client.PostAsync($"savefile/import/{viewerId}", content);
 
         this.ApiContext
             .PlayerStoryState
-            .Single(x => x.DeviceAccountId == DeviceAccountId && x.StoryId == 110313011)
+            .Single(x => x.ViewerId == ViewerId && x.StoryId == 110313011)
             .StoryType
             .Should()
             .Be(StoryTypes.Chara);
 
         this.ApiContext
             .PlayerStoryState
-            .Single(x => x.DeviceAccountId == DeviceAccountId && x.StoryId == 210091011)
+            .Single(x => x.ViewerId == ViewerId && x.StoryId == 210091011)
             .StoryType
             .Should()
             .Be(StoryTypes.Dragon);
@@ -175,10 +169,7 @@ public class SavefileImportTest : TestFixture
     [Fact]
     public async Task Import_IsIdempotent()
     {
-        long viewerId = this.ApiContext
-            .PlayerUserData
-            .Single(x => x.DeviceAccountId == DeviceAccountId)
-            .ViewerId;
+        long viewerId = this.ApiContext.PlayerUserData.Single(x => x.ViewerId == ViewerId).ViewerId;
 
         HttpContent content = PrepareSavefileRequest();
 

commit b92923ba79aedaf822fb4fd9026ece211833fa43

Fix save import time not updating (#501)
Seems to have been broken in #500 going by the latest save import time
in the db, but can't find where this was being done before then either 😲

DragaliaAPI/Services/Game/SavefileService.cs
@@ -436,6 +436,8 @@ public class SavefileService : ISavefileService
                 stopwatch.Elapsed.TotalMilliseconds
             );
 
+            player.UserData.LastSaveImportTime = DateTimeOffset.UtcNow;
+
             await apiContext.SaveChangesAsync();
             await transaction.CommitAsync();
 

DragaliaAPI.Integration.Test/Other/SavefileImportTest.cs
@@ -2,6 +2,7 @@
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using DragaliaAPI.Shared.Json;
+using Microsoft.EntityFrameworkCore;
 
 namespace DragaliaAPI.Integration.Test.Other;
 
@@ -50,7 +51,11 @@ public class SavefileImportTest : TestFixture
     public async Task Import_LoadIndexReturnsImportedSavefile()
     {
         string savefileJson = File.ReadAllText(Path.Join("Data", "endgame_savefile.json"));
-        long viewerId = this.ApiContext.PlayerUserData.Single(x => x.ViewerId == ViewerId).ViewerId;
+        long viewerId = this.ApiContext
+            .PlayerUserData
+            .AsNoTracking()
+            .Single(x => x.ViewerId == ViewerId)
+            .ViewerId;
 
         LoadIndexData savefile = JsonSerializer
             .Deserialize<DragaliaResponse<LoadIndexData>>(savefileJson, ApiJsonOptions.Instance)!
@@ -64,6 +69,14 @@ public class SavefileImportTest : TestFixture
             content
         );
         importResponse.StatusCode.Should().Be(HttpStatusCode.NoContent);
+
+        this.ApiContext
+            .PlayerUserData
+            .AsNoTracking()
+            .Single(x => x.ViewerId == ViewerId)
+            .LastSaveImportTime
+            .Should()
+            .BeCloseTo(DateTimeOffset.UtcNow, TimeSpan.FromMinutes(1));
 
         LoadIndexData storedSavefile = (
             await this.Client.PostMsgpack<LoadIndexData>("load/index", new LoadIndexRequest())