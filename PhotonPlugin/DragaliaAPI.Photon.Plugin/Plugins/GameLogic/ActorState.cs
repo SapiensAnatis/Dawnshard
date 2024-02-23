3ce88bc5e7eef0775206fbc8074796a8813f0f5e
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2024-02-23 20:55:08 +0000

    Fix a few occurrences of KeyNotFoundException in the plugin (#666)
    Most of these are to do with the logic to bring the minimum
    GoToIngameState forward when an actor leaves. Typically the cause is
    iterating through `PluginHost.GameActors` and then indexing each
    element's `ActorNr` into a state dictionary... we remove the actor from
    the state dictionary on leave, but the actor is not always removed from
    the plugin host list at the same time.
    
    Generally fixed by looping through the dictionaries instead of
    `PluginHost.GameActors` in `RaisePartyEvent` and
    `RaiseCharacterDataEvent`
    
    Additionally:
    - Refactor all `GoToIngameState` logic into `GoToIngameStateManager` to
    make `GameLogicPlugin` more manageable
    - Change the raising of the character data event into `GoToIngameState =
    4`, based on the enum in the il2cpp.dll specifying this value:
    
      ```cs
      public enum GoToIngameState
      {
    	  None,
    	  TryGoToIngame,
    	  PreCheckParty,
    	  CheckParty,
    	  PostCheckParty
      }
      ```
    
    NOTE: for leave logic to become effective, adjust
    `MinimumTimeout`/`MaximumTimeout` down in bin_Win64/PhotonServer.config

--- PhotonPlugin/DragaliaAPI.Photon.Plugin/Plugins/GameLogic/ActorState.cs
+++ PhotonPlugin/DragaliaAPI.Photon.Plugin/Plugins/GameLogic/ActorState.cs
@@ -5,13 +5,6 @@ namespace DragaliaAPI.Photon.Plugin.Plugins.GameLogic
 {
     internal class ActorState
     {
-        public HeroParamData HeroParamData { get; set; }
-
-        public int HeroParamCount =>
-            this.HeroParamData is null ? 0 : this.HeroParamData.HeroParamLists.First().Count();
-
-        public int MemberCount { get; set; }
-
         public bool Dead { get; set; }
 
         public bool Ready { get; set; }
