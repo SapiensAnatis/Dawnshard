dc8bbf10e70bf88c655dc6431dc194c3c022f0b9
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2024-03-11 20:31:57 +0000

    Fix tests

--- DragaliaAPI/DragaliaAPI/Mapping/Mapperly/CharaMapper.cs
+++ DragaliaAPI/DragaliaAPI/Mapping/Mapperly/CharaMapper.cs
@@ -10,10 +10,4 @@ public static partial class CharaMapper
     [MapperRequiredMapping(RequiredMappingStrategy.Target)]
     [MapperIgnoreTarget(nameof(CharaList.StatusPlusCount))]
     public static partial CharaList ToCharaList(this DbPlayerCharaData dbModel);
-
-    [MapperRequiredMapping(RequiredMappingStrategy.Target)]
-    [MapperIgnoreTarget(nameof(CharaList.StatusPlusCount))]
-    public static partial IQueryable<CharaList> ProjectToCharaList(
-        this IQueryable<DbPlayerCharaData> query
-    );
 }
