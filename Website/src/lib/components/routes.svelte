6aa09b559f5428813dbf095f6e998640111a6359
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2024-05-09 19:21:25 +0100

    New website: homepage (#789)
    I am looking to rewrite the existing webpage in a JavaScript framework
    for a few reasons:
    
    - Blazor Server requires a constant websocket connection, which can be
    prone to dropping.
    - The original in-game webviews used JavaScript and assorted tricks such
    as manipulating the window hash (see #494). It will be easier to achieve
    parity with a JavaScript framework.
    - I believe the depth of customization and the ecosystem around JS apps
    will be needed if we want to pursue a custom component library for fully
    replicating the style of the in-game webviews.
    
    Point 1 and point 2 to a degree could be counteracted using Blazor
    WebAssembly, but the initial load times on this are unacceptably long
    especially for the in-game webviews.
    
    I've chosen Svelte because I wanted to try and learn it and enjoyed what
    I saw. I think that React would add too much complexity for a website
    that ultimately does not need to do a lot.
    
    This PR contains the homepage, which just sets outs the projects aims,
    status and has a how-to-play guide. It won't currently deploy anywhere
    and there is no CI. I am just making a pull request now so that I don't
    have to make one when the rewrite is 'done' with 1000+ changed files.

---
+++ Website/src/lib/components/routes.svelte
@@ -0,0 +1,15 @@
+<script>
+	import { routeGroups } from '../routes';
+	import { Button } from '$shadcn/components/ui/button';
+	import { cn } from '$shadcn/utils.js';
+</script>
+
+{#each routeGroups as routeGroup}
+	<h2 class="text-m scroll-m-20 font-semibold tracking-tight">{routeGroup.title}</h2>
+	{#each routeGroup.routes as route}
+		<Button href={route.href} variant="ghost" size="sm" class={cn('justify-start')}>
+			<svelte:component this={route.icon} class="mr-2 size-4" aria-hidden="true" />
+			{route.title}
+		</Button>
+	{/each}
+{/each}
---
+++ Website/src/routes/(main)/sideNav.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Routes from '$lib/components/routes.svelte';
+</script>
+
+<div id="navigation" class="hidden flex-col gap-4 py-2 md:flex">
+	<nav class="grid gap-1 px-2">
+		<Routes />
+	</nav>
+</div>
+
+<style>
+	#navigation {
+		border-right: 1px solid;
+		border-color: var(--divider);
+		position: fixed;
+		height: 100%;
+		top: var(--header-height);
+		width: var(--navigation-width);
+		padding: 1rem 0 0 1rem;
+	}
+</style>
