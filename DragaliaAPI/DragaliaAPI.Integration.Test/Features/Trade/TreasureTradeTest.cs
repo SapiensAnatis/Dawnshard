SapiensAnatis/Dawnshard

commit 64e4d66e14ec8911129d825246e7df227e67e72d

Add limit for dragons (#792)
Starts enforcing the maximum dragon limit, with excess going to the
present box. Closes
https://github.com/SapiensAnatis/Dawnshard/issues/670

On save import, the dragon list will be trimmed, taking the first N
dragons according to the `$.user_data.max_dragon_quantity`. This itself
is capped at 500 to prevent people from editing it to `int.MaxValue`.
Existing saves will not be affected unless they are re-imported.

Additionally includes some refactoring of `RewardService` and the
summoning logic. The existing 'grant multiple character/dragon' logic
has been extracted into `RewardService` as an implementation of a new
`IBatchRewardHandler`. `SummonService` now calls this from a support
`UnitService` that handles the unique requirement of needing to know
whether an added dragon was new, since this cannot be known from the
reward grant result.

DragaliaAPI/DragaliaAPI/Features/Reward/Handlers/DragonHandler.cs
@@ -1,0 +1,120 @@
+using DragaliaAPI.Database;
+using DragaliaAPI.Database.Entities;
+using DragaliaAPI.Extensions;
+using DragaliaAPI.Shared.Definitions.Enums;
+using DragaliaAPI.Shared.PlayerDetails;
+using Microsoft.EntityFrameworkCore;
+
+namespace DragaliaAPI.Features.Reward.Handlers;
+
+public partial class DragonHandler(
+    ApiContext apiContext,
+    IPlayerIdentityService playerIdentityService,
+    ILogger<DragonHandler> logger
+) : IRewardHandler, IBatchRewardHandler
+{
+    public IReadOnlyList<EntityTypes> SupportedTypes { get; } = [EntityTypes.Dragon];
+
+    public async Task<GrantReturn> Grant(Entity entity)
+    {
+        Dragons dragon = (Dragons)entity.Id;
+        if (!Enum.IsDefined(dragon))
+        {
+            Log.InvalidDragonId(logger, entity, dragon);
+            return GrantReturn.FailError();
+        }
+
+        int storageSpace = await apiContext
+            .PlayerUserData.Select(x => x.MaxDragonQuantity)
+            .FirstAsync();
+        int dragonCount = await apiContext.PlayerDragonData.CountAsync();
+
+        if (dragonCount >= storageSpace)
+        {
+            return GrantReturn.Limit();
+        }
+
+        apiContext.PlayerDragonData.Add(
+            new DbPlayerDragonData(playerIdentityService.ViewerId, (Dragons)entity.Id)
+        );
+
+        if (
+            !apiContext.PlayerDragonReliability.Local.Any(x => x.DragonId == dragon)
+            && !await apiContext.PlayerDragonReliability.AnyAsync(x => x.DragonId == dragon)
+        )
+        {
+            apiContext.PlayerDragonReliability.Add(
+                new DbPlayerDragonReliability(playerIdentityService.ViewerId, dragon)
+            );
+        }
+
+        return GrantReturn.Added();
+    }
+
+    public async Task<IDictionary<TKey, GrantReturn>> GrantRange<TKey>(
+        IDictionary<TKey, Entity> entities
+    )
+        where TKey : struct
+    {
+        int storageSpace = await apiContext
+            .PlayerUserData.Select(x => x.MaxDragonQuantity)
+            .FirstAsync();
+        int dragonCount = await apiContext.PlayerDragonData.CountAsync();
+
+        HashSet<Dragons> ownedReliabilities = await apiContext
+            .PlayerDragonReliability.Where(x =>
+                entities.Values.Select(y => (Dragons)y.Id).Contains(x.DragonId)
+            )
+            .Select(x => x.DragonId)
+            .ToHashSetAsync();
+
+        Dictionary<TKey, GrantReturn> resultDict = [];
+
+        foreach ((TKey key, Entity entity) in entities)
+        {
+            Dragons dragon = (Dragons)entity.Id;
+
+            if (!Enum.IsDefined(dragon))
+            {
+                Log.InvalidDragonId(logger, entity, dragon);
+                resultDict.Add(key, GrantReturn.FailError());
+                continue;
+            }
+
+            if (dragonCount >= storageSpace)
+            {
+                resultDict.Add(key, GrantReturn.Limit());
+                continue;
+            }
+
+            apiContext.PlayerDragonData.Add(
+                new DbPlayerDragonData(playerIdentityService.ViewerId, dragon)
+            );
+
+            if (
+                !ownedReliabilities.Contains(dragon)
+                && !apiContext.PlayerDragonReliability.Local.Any(x => x.DragonId == dragon)
+            )
+            {
+                apiContext.PlayerDragonReliability.Add(
+                    new DbPlayerDragonReliability(playerIdentityService.ViewerId, dragon)
+                );
+                ownedReliabilities.Add(dragon);
+            }
+
+            resultDict.Add(key, GrantReturn.Added());
+            dragonCount++;
+        }
+
+        return resultDict;
+    }
+
+    private static partial class Log
+    {
+        [LoggerMessage(
+            LogLevel.Error,
+            "Entity {Entity} is not a valid dragon entity: {DragonId} is not a dragon ID"
+        )]
+        public static partial void InvalidDragonId(ILogger logger, Entity entity, Dragons dragonId);
+    }
+}

DragaliaAPI/DragaliaAPI/Features/Reward/IRewardService.cs
@@ -1,6 +1,7 @@
 ﻿using DragaliaAPI.Database.Entities;
 using DragaliaAPI.Models.Generated;
 using DragaliaAPI.Shared.Definitions.Enums;
+using JetBrains.Annotations;
 
 namespace DragaliaAPI.Features.Reward;
 
@@ -13,6 +14,7 @@ public interface IRewardService
     /// <returns>
     /// An enum indicating the result of the add operation.
     /// </returns>
+    [MustUseReturnValue]
     Task<RewardGrantResult> GrantReward(Entity entity);
 
     /// <summary>
@@ -22,6 +24,7 @@ public interface IRewardService
     /// <returns>The task.</returns>
     Task GrantRewards(IEnumerable<Entity> entities);
 
+    [MustUseReturnValue]
     Task<(RewardGrantResult Result, DbTalisman? Talisman)> GrantTalisman(
         Talismans id,
         int abilityId1,
@@ -33,4 +36,9 @@ public interface IRewardService
 
     EntityResult GetEntityResult();
     IEnumerable<ConvertedEntity> GetConvertedEntityList();
+
+    Task<IDictionary<TKey, RewardGrantResult>> GrantRewardsWithBatchHandler<TKey>(
+        IDictionary<TKey, Entity> entities
+    )
+        where TKey : struct;
 }

DragaliaAPI/DragaliaAPI/Features/Reward/RewardService.cs
@@ -4,18 +4,17 @@ using DragaliaAPI.Features.Reward.Handlers;
 using DragaliaAPI.Features.Talisman;
 using DragaliaAPI.Models.Generated;
 using DragaliaAPI.Shared.Definitions.Enums;
+using StackExchange.Redis;
 
 namespace DragaliaAPI.Features.Reward;
 
 public class RewardService(
     ILogger<RewardService> logger,
     IUnitRepository unitRepository,
-    IEnumerable<IRewardHandler> rewardHandlers
+    IEnumerable<IRewardHandler> rewardHandlers,
+    IEnumerable<IBatchRewardHandler> batchRewardHandlers
 ) : IRewardService
 {
-    private readonly List<Entity> discardedEntities = new();
-    private readonly List<Entity> presentEntites = new();
-    private readonly List<Entity> presentLimitEntities = new();
     private readonly List<Entity> newEntities = new();
     private readonly List<ConvertedEntity> convertedEntities = new();
 
@@ -29,43 +28,80 @@ public class RewardService(
 
         logger.LogTrace("Granting reward {@rewardEntity}", entity);
 
-        RewardGrantResult result = await GrantRewardInternal(entity);
+        IRewardHandler handler = this.GetHandler(entity.Type);
 
-        logger.LogTrace("Result: {result}", result);
+        GrantReturn grantReturn = await handler.Grant(entity);
+        await ProcessGrantResult(grantReturn, entity);
 
-        return result;
+        logger.LogTrace("Result: {result}", grantReturn.Result);
+
+        return grantReturn.Result;
     }
 
     public async Task GrantRewards(IEnumerable<Entity> entities)
     {
-        entities = entities.ToList();
-        logger.LogTrace("Granting rewards: {@rewards}", entities);
-
-        if (!entities.TryGetNonEnumeratedCount(out int count))
-            count = 0;
-
-        List<RewardGrantResult> results = new(count);
         foreach (Entity entity in entities)
         {
-            RewardGrantResult result = await GrantRewardInternal(entity);
-            results.Add(result);
-        }
+            IRewardHandler handler = this.GetHandler(entity.Type);
 
-        logger.LogTrace("Results: {@results}", results);
+            GrantReturn grantReturn = await handler.Grant(entity);
+            await ProcessGrantResult(grantReturn, entity);
+        }
     }
 
-    private async Task<RewardGrantResult> GrantRewardInternal(Entity entity)
+    public async Task<IDictionary<TKey, RewardGrantResult>> GrantRewardsWithBatchHandler<TKey>(
+        IDictionary<TKey, Entity> entities
+    )
+        where TKey : struct
     {
-        IRewardHandler handler = this.GetHandler(entity.Type);
-        GrantReturn grantReturn = await handler.Grant(entity);
+        Dictionary<TKey, RewardGrantResult> result = [];
 
+        IEnumerable<(EntityTypes type, Dictionary<TKey, Entity>)> grouping = entities.GroupBy(
+            x => x.Value.Type,
+            (type, group) => (type, group.ToDictionary())
+        );
+
+        foreach ((EntityTypes type, Dictionary<TKey, Entity> dictionary) in grouping)
+        {
+            if (
+                batchRewardHandlers.FirstOrDefault(x => x.SupportedTypes.Contains(type))
+                is not { } batchRewardHandler
+            )
+            {
+                throw new NotSupportedException(
+                    $"Entities of type {type} are not supported in this method"
+                );
+            }
+
+            IDictionary<TKey, GrantReturn> batchResult = await batchRewardHandler.GrantRange(
+                dictionary
+            );
+
+            foreach ((TKey key, GrantReturn grantReturn) in batchResult)
+            {
+                await ProcessGrantResult(grantReturn, dictionary[key]);
+                result.Add(key, grantReturn.Result);
+            }
+        }
+
+        return result;
+    }
+
+    private async Task ProcessGrantResult(GrantReturn grantReturn, Entity entity)
+    {
         switch (grantReturn.Result)
         {
             case RewardGrantResult.Added:
                 this.newEntities.Add(entity);
                 break;
             case RewardGrantResult.Converted:
-                ArgumentNullException.ThrowIfNull(grantReturn.ConvertedEntity);
+            {
+                if (grantReturn.ConvertedEntity is null)
+                {
+                    throw new InvalidOperationException(
+                        "RewardGrantResult.Converted was returned, but converted entity was null!"
+                    );
+                }
 
                 this.convertedEntities.Add(
                     new ConvertedEntity(entity, grantReturn.ConvertedEntity)
@@ -74,33 +110,23 @@ public class RewardService(
                     .Grant(grantReturn.ConvertedEntity);
 
                 break;
-            case RewardGrantResult.Discarded:
-                this.discardedEntities.Add(entity);
-                break;
-            case RewardGrantResult.GiftBoxDiscarded:
-                this.presentLimitEntities.Add(entity);
-                break;
-            case RewardGrantResult.GiftBox:
-                this.presentEntites.Add(entity);
-                break;
+            }
             case RewardGrantResult.Limit:
+            case RewardGrantResult.Discarded:
                 break;
             case RewardGrantResult.FailError:
                 logger.LogError("Granting of entity {@entity} failed.", entity);
                 throw new InvalidOperationException("Failed to grant reward");
             default:
-                throw new ArgumentOutOfRangeException(
-                    string.Empty,
-                    "RewardGrantResult out of range"
+                throw new InvalidOperationException(
+                    $"RewardGrantResult {grantReturn.Result} out of range"
                 );
         }
-
-        return grantReturn.Result;
     }
 
     private IRewardHandler GetHandler(EntityTypes type)
     {
-        IRewardHandler? handler = rewardHandlers.SingleOrDefault(x =>
+        IRewardHandler? handler = rewardHandlers.FirstOrDefault(x =>
             x.SupportedTypes.Contains(type)
         );
 
@@ -156,13 +182,6 @@ public class RewardService(
         {
             NewGetEntityList = newEntities.Select(x => x.ToDuplicateEntityList()),
             ConvertedEntityList = convertedEntities.Select(x => x.ToConvertedEntityList()),
-            OverDiscardEntityList = discardedEntities.Select(x => x.ToBuildEventRewardEntityList()),
-            OverPresentEntityList = this.presentEntites.Select(x =>
-                x.ToBuildEventRewardEntityList()
-            ),
-            OverPresentLimitEntityList = this.presentLimitEntities.Select(x =>
-                x.ToBuildEventRewardEntityList()
-            ),
         };
     }
 

commit 1d8ba622996ee2738b47a42df02190e166fb8909

Fix DragonHandler ignoring multiple dragons
More fallout from #792 - I forgot that you can trade for multiple
dragons at once in treasure trade. After #792 this only gives 1 dragon
and scams the player. This is actually quite tricky to handle since
each dragon could put you over the limit.

This change special-cases dragon trades and splits them up to be sent
to the batch reward handler which can process each one individually.
To accomplish this the BatchGrantReward method has been expanded to
use a fallback for non-batchable entities instead of just throwing.

DragaliaAPI/DragaliaAPI/Features/Present/IPresentService.cs
@@ -12,4 +12,5 @@ public interface IPresentService
     void AddPresent(Present present);
 
     void AddPresent(IEnumerable<Present> presents);
+    IEnumerable<AtgenBuildEventRewardEntityList> GetTrackedPresentList();
 }

DragaliaAPI/DragaliaAPI/Features/Present/PresentService.cs
@@ -1,3 +1,4 @@
+using DragaliaAPI.Database;
 using DragaliaAPI.Models.Generated;
 using DragaliaAPI.Shared.PlayerDetails;
 using Microsoft.EntityFrameworkCore;
@@ -7,28 +8,20 @@ namespace DragaliaAPI.Features.Present;
 /// <summary>
 /// Base present service to be used by other features to check/add present data.
 /// </summary>
-public class PresentService : IPresentService
+public class PresentService(
+    IPresentRepository presentRepository,
+    IPlayerIdentityService playerIdentityService,
+    ApiContext apiContext
+) : IPresentService
 {
-    private readonly IPresentRepository presentRepository;
-    private readonly IPlayerIdentityService playerIdentityService;
-
-    public PresentService(
-        IPresentRepository presentRepository,
-        IPlayerIdentityService playerIdentityService
-    )
-    {
-        this.presentRepository = presentRepository;
-        this.playerIdentityService = playerIdentityService;
-    }
-
     public async Task<PresentNotice> GetPresentNotice()
     {
         return new()
         {
-            PresentCount = await this.presentRepository.Presents.CountAsync(x =>
+            PresentCount = await presentRepository.Presents.CountAsync(x =>
                 x.ReceiveLimitTime == null
             ),
-            PresentLimitCount = await this.presentRepository.Presents.CountAsync(x =>
+            PresentLimitCount = await presentRepository.Presents.CountAsync(x =>
                 x.ReceiveLimitTime != null
             ),
         };
@@ -41,8 +34,15 @@ public class PresentService : IPresentService
 
     public void AddPresent(IEnumerable<Present> presents)
     {
-        this.presentRepository.AddPlayerPresents(
-            presents.Select(x => x.ToEntity(this.playerIdentityService.ViewerId))
+        presentRepository.AddPlayerPresents(
+            presents.Select(x => x.ToEntity(playerIdentityService.ViewerId))
         );
     }
+
+    public IEnumerable<AtgenBuildEventRewardEntityList> GetTrackedPresentList() =>
+        apiContext.PlayerPresents.Local.Select(x => new AtgenBuildEventRewardEntityList(
+            x.EntityType,
+            x.EntityId,
+            x.EntityQuantity
+        ));
 }

DragaliaAPI/DragaliaAPI/Features/Reward/Handlers/DragonHandler.cs
@@ -16,6 +16,14 @@ public partial class DragonHandler(
 
     public async Task<GrantReturn> Grant(Entity entity)
     {
+        if (entity.Quantity > 1)
+        {
+            throw new ArgumentException(
+                "Cannot process dragons with quantity >1 in single reward handler due to the possibility of multiple results. Use the batch handler instead.",
+                nameof(entity)
+            );
+        }
+
         Dragons dragon = (Dragons)entity.Id;
         if (!Enum.IsDefined(dragon))
         {

DragaliaAPI/DragaliaAPI/Features/Reward/IRewardService.cs
@@ -37,7 +37,7 @@ public interface IRewardService
     EntityResult GetEntityResult();
     IEnumerable<ConvertedEntity> GetConvertedEntityList();
 
-    Task<IDictionary<TKey, RewardGrantResult>> GrantRewardsWithBatchHandler<TKey>(
+    Task<IDictionary<TKey, RewardGrantResult>> BatchGrantRewards<TKey>(
         IDictionary<TKey, Entity> entities
     )
         where TKey : struct;

DragaliaAPI/DragaliaAPI/Features/Reward/RewardService.cs
@@ -48,7 +48,7 @@ public class RewardService(
         }
     }
 
-    public async Task<IDictionary<TKey, RewardGrantResult>> GrantRewardsWithBatchHandler<TKey>(
+    public async Task<IDictionary<TKey, RewardGrantResult>> BatchGrantRewards<TKey>(
         IDictionary<TKey, Entity> entities
     )
         where TKey : struct
@@ -63,23 +63,30 @@ public class RewardService(
         foreach ((EntityTypes type, Dictionary<TKey, Entity> dictionary) in grouping)
         {
             if (
-                batchRewardHandlers.FirstOrDefault(x => x.SupportedTypes.Contains(type))
-                is not { } batchRewardHandler
+                batchRewardHandlers.FirstOrDefault(x => x.SupportedTypes.Contains(type)) is
+                { } batchRewardHandler
             )
             {
-                throw new NotSupportedException(
-                    $"Entities of type {type} are not supported in this method"
+                IDictionary<TKey, GrantReturn> batchResult = await batchRewardHandler.GrantRange(
+                    dictionary
                 );
+
+                foreach ((TKey key, GrantReturn grantReturn) in batchResult)
+                {
+                    await ProcessGrantResult(grantReturn, dictionary[key]);
+                    result.Add(key, grantReturn.Result);
+                }
             }
-
-            IDictionary<TKey, GrantReturn> batchResult = await batchRewardHandler.GrantRange(
-                dictionary
-            );
-
-            foreach ((TKey key, GrantReturn grantReturn) in batchResult)
+            else
             {
-                await ProcessGrantResult(grantReturn, dictionary[key]);
-                result.Add(key, grantReturn.Result);
+                IRewardHandler handler = this.GetHandler(type);
+
+                foreach ((TKey key, Entity entity) in dictionary)
+                {
+                    GrantReturn grantReturn = await handler.Grant(entity);
+                    await ProcessGrantResult(grantReturn, entity);
+                    result.Add(key, grantReturn.Result);
+                }
             }
         }
 

DragaliaAPI/DragaliaAPI/Features/Summoning/SummonService.cs
@@ -479,13 +479,9 @@ public sealed partial class SummonService(
             returnedResult.Add(processedResult);
         }
 
-        IEnumerable<AtgenBuildEventRewardEntityList> overPresentEntityList = apiContext
-            .PlayerPresents.Local.Where(x => x.EntityType == EntityTypes.Dragon)
-            .Select(x => new AtgenBuildEventRewardEntityList(
-                x.EntityType,
-                x.EntityId,
-                x.EntityQuantity
-            ));
+        IEnumerable<AtgenBuildEventRewardEntityList> overPresentEntityList = presentService
+            .GetTrackedPresentList()
+            .Where(x => x.EntityType == EntityTypes.Dragon);
 
         return (
             returnedResult,

DragaliaAPI/DragaliaAPI/Features/Summoning/UnitService.cs
@@ -34,7 +34,7 @@ public class UnitService(
             .ToDictionary();
 
         IDictionary<int, RewardGrantResult> outputRewardDict =
-            await rewardService.GrantRewardsWithBatchHandler(inputRewardDict);
+            await rewardService.BatchGrantRewards(inputRewardDict);
 
         List<CharaNewCheckResult> result = [];
 
@@ -55,7 +55,7 @@ public class UnitService(
             .ToDictionary();
 
         IDictionary<int, RewardGrantResult> outputRewardDict =
-            await rewardService.GrantRewardsWithBatchHandler(inputRewardDict);
+            await rewardService.BatchGrantRewards(inputRewardDict);
 
         IEnumerable<Present.Present> presentsToAdd = outputRewardDict
             .Where(kvp => kvp.Value == RewardGrantResult.Limit)

DragaliaAPI/DragaliaAPI/Features/Trade/ITradeService.cs
@@ -20,4 +20,5 @@ public interface ITradeService
         IEnumerable<AtgenNeedUnitList>? needUnitList = null
     );
     Task DoAbilityCrestTrade(int id, int count);
+    EntityResult GetEntityResult();
 }

DragaliaAPI/DragaliaAPI/Features/Trade/TradeService.cs
@@ -164,24 +164,53 @@ public class TradeService(
             );
         }
 
-        RewardGrantResult result = await rewardService.GrantReward(
-            new(
-                trade.DestinationEntityType,
-                trade.DestinationEntityId,
-                trade.DestinationEntityQuantity * count,
-                trade.DestinationLimitBreakCount
-            )
+        Dictionary<int, Entity> entities;
+
+        if (trade.DestinationEntityType == EntityTypes.Dragon)
+        {
+            // We must flatten dragons out into multiple entities, since each grant could have a different result
+            entities = Enumerable
+                .Repeat(
+                    new Entity(
+                        trade.DestinationEntityType,
+                        trade.DestinationEntityId,
+                        1,
+                        trade.DestinationLimitBreakCount
+                    ),
+                    trade.DestinationEntityQuantity * count
+                )
+                .Select((x, index) => KeyValuePair.Create(index, x))
+                .ToDictionary();
+        }
+        else
+        {
+            entities = new()
+            {
+                [1] = new Entity(
+                    trade.DestinationEntityType,
+                    trade.DestinationEntityId,
+                    trade.DestinationEntityQuantity * count,
+                    trade.DestinationLimitBreakCount
+                )
+            };
+        }
+
+        IDictionary<int, RewardGrantResult> batchResult = await rewardService.BatchGrantRewards(
+            entities
         );
 
-        if (result == RewardGrantResult.Limit)
+        foreach ((_, RewardGrantResult result) in batchResult)
         {
-            presentService.AddPresent(
-                new Present.Present(
-                    PresentMessage.TreasureTrade,
-                    trade.DestinationEntityType,
-                    trade.DestinationEntityId
-                )
-            );
+            if (result == RewardGrantResult.Limit)
+            {
+                presentService.AddPresent(
+                    new Present.Present(
+                        PresentMessage.TreasureTrade,
+                        trade.DestinationEntityType,
+                        trade.DestinationEntityId
+                    )
+                );
+            }
         }
 
         await tradeRepository.AddTrade(tradeType, tradeId, count, DateTimeOffset.UtcNow);
@@ -216,5 +245,13 @@ public class TradeService(
         );
 
         await tradeRepository.AddTrade(TradeType.AbilityCrest, id, count);
+    }
+
+    public EntityResult GetEntityResult()
+    {
+        EntityResult result = rewardService.GetEntityResult();
+        result.OverPresentEntityList = presentService.GetTrackedPresentList();
+
+        return result;
     }
 }

DragaliaAPI/DragaliaAPI/Features/Trade/TreasureTradeController.cs
@@ -44,6 +44,7 @@ public class TreasureTradeController(
         resp.UpdateDataList = await updateDataService.SaveChangesAsync(cancellationToken);
         resp.TreasureTradeAllList = tradeService.GetCurrentTreasureTradeList();
         resp.UserTreasureTradeList = await tradeService.GetUserTreasureTradeList();
+        resp.EntityResult = tradeService.GetEntityResult();
 
         return Ok(resp);
     }

DragaliaAPI/DragaliaAPI.Integration.Test/Features/Trade/TreasureTradeTest.cs
@@ -113,4 +113,38 @@ public class TreasureTradeTest : TestFixture
         response.UpdateDataList.Should().NotBeNull();
         response.UpdateDataList.WeaponSkinList.Should().Contain(x => x.WeaponSkinId == 30159921);
     }
+
+    [Fact]
+    public async Task Trade_MultiDragon_AlmostFull_Trades()
+    {
+        this.ApiContext.PlayerDragonData.ExecuteDelete();
+        this.ApiContext.PlayerUserData.ExecuteUpdate(e =>
+            e.SetProperty(x => x.MaxDragonQuantity, 2)
+        );
+
+        int highBrunhildaTrade = 10030302;
+
+        TreasureTradeTradeResponse response = (
+            await Client.PostMsgpack<TreasureTradeTradeResponse>(
+                "treasure_trade/trade",
+                new TreasureTradeTradeRequest(1003, highBrunhildaTrade, null, 4)
+            )
+        ).Data;
+
+        response.UpdateDataList.DragonList.Should().HaveCount(2);
+        response.EntityResult.OverPresentEntityList.Should().HaveCount(2);
+
+        this.ApiContext.PlayerDragonData.ToList()
+            .Should()
+            .HaveCount(2)
+            .And.AllSatisfy(x => x.DragonId.Should().Be(Dragons.HighBrunhilda));
+        this.ApiContext.PlayerPresents.ToList()
+            .Should()
+            .HaveCount(2)
+            .And.AllSatisfy(x =>
+            {
+                x.EntityType.Should().Be(EntityTypes.Dragon);
+                x.EntityId.Should().Be((int)Dragons.HighBrunhilda);
+            });
+    }
 }