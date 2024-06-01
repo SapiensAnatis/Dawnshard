98e033a54a4755ea0d3d9d4fa0ce522442a5ded2
Jay Malhotra <jay0@me.com>
2024-06-01 14:30:37 +0100

    Add migration

---
+++ DragaliaAPI/DragaliaAPI.Database/Migrations/20240601133017_summon_pity_rates.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DragaliaAPI.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class summon_pity_rates : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Pity",
+                table: "PlayerBannerData");
+
+            migrationBuilder.AddColumn<int>(
+                name: "SummonCountSinceLastFiveStar",
+                table: "PlayerBannerData",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SummonCountSinceLastFiveStar",
+                table: "PlayerBannerData");
+
+            migrationBuilder.AddColumn<byte>(
+                name: "Pity",
+                table: "PlayerBannerData",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+    }
+}
