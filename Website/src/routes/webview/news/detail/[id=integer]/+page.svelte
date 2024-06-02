a02aa2036650ebc5b092f6a7e225b7e18047ad06
Jay Malhotra <jay0@me.com>
2024-06-02 21:47:57 +0100

    Remove promise streaming in detail page

--- Website/src/routes/webview/news/detail/[id=integer]/+page.svelte
+++ Website/src/routes/webview/news/detail/[id=integer]/+page.svelte
@@ -3,7 +3,6 @@
   import type { PageData } from './$types';
   import { afterNavigate } from '$app/navigation';
   import { Button } from '$shadcn/components/ui/button';
-  import { Skeleton } from '$shadcn/components/ui/skeleton';
   import ChevronLeft from 'lucide-svelte/icons/chevron-left';
 
   let previousPage: string = '/webview/news/1';
@@ -13,6 +12,8 @@
   });
 
   export let data: PageData;
+
+  let item = data.newsItem;
 </script>
 
 <div class="p-4">
@@ -21,48 +22,30 @@
     Back to news
   </Button>
   <div class="mt-4">
-    {#await data.newsPromise}
-      <Skeleton class="h-[200px] w-full" />
-      <br />
-      <Skeleton class="mb-2 h-6 w-[80%]" />
-      <Skeleton class="h-4 w-[50%]" />
-      <br />
-      <div class="flex flex-col gap-1">
-        <Skeleton class="h-4 w-[90%]" />
-        <Skeleton class="h-4 w-[95%]" />
-        <Skeleton class="h-4 w-[80%]" />
-        <Skeleton class="h-4 w-[86%]" />
+    {#if item.headerImageSrc}
+      <div class="mt-4 flex flex-row items-center justify-center">
+        <Image class="rounded-md" src={item.headerImageSrc} layout="fullWidth" height={200} />
       </div>
-    {:then item}
-      {#if !item}
-        <p>Failed to load news item!</p>
-      {:else}
-        {#if item.headerImageSrc}
-          <div class="mt-4 flex flex-row items-center justify-center">
-            <Image class="rounded-md" src={item.headerImageSrc} layout="fullWidth" height={200} />
-          </div>
-          <br />
-        {/if}
-        <div>
-          <h1 class="text-2xl font-semibold">
-            {item.headline}
-          </h1>
-          <p class="text-sm text-gray-500">
-            {item.date.toLocaleString(undefined, {
-              dateStyle: 'long',
-              timeStyle: 'short'
-            })}
-          </p>
-          <br />
-          <!-- Trusted input from API server - XSS is unlikely without server being compromised -->
-          <!-- eslint-disable-next-line svelte/no-at-html-tags -->
-          <p>{@html item.description}</p>
-          <br />
-          {#if item.bodyImageSrc}
-            <Image src={item.bodyImageSrc} layout="fullWidth" class="w-full" />
-          {/if}
-        </div>
+      <br />
+    {/if}
+    <div>
+      <h1 class="text-2xl font-semibold">
+        {item.headline}
+      </h1>
+      <p class="text-sm text-gray-500">
+        {item.date.toLocaleString(undefined, {
+          dateStyle: 'long',
+          timeStyle: 'short'
+        })}
+      </p>
+      <br />
+      <!-- Trusted input from API server - XSS is unlikely without server being compromised -->
+      <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+      <p>{@html item.description}</p>
+      <br />
+      {#if item.bodyImageSrc}
+        <Image src={item.bodyImageSrc} layout="fullWidth" class="w-full" />
       {/if}
-    {/await}
+    </div>
   </div>
 </div>
--- Website/src/routes/webview/news/detail/[id=integer]/+page.ts
+++ Website/src/routes/webview/news/detail/[id=integer]/+page.ts
@@ -6,20 +6,13 @@ export const load: PageLoad = async ({ fetch, params }) => {
   const id = Number.parseInt(params.id) || 1;
   const requestUrl = new URL(`news/${id}`, PUBLIC_DAWNSHARD_API_URL);
 
-  const newsPromise = fetch(requestUrl)
-    .then(async (response) => {
-      if (!response.ok) {
-        throw new Error(`News API call failed with status ${response.status}`);
-      }
+  const response = await fetch(requestUrl);
 
-      return newsItemSchema.parse(await response.json());
-    })
-    .catch((err) => {
-      console.error('Failed to load news:', err);
-      return null;
-    });
+  if (!response.ok) {
+    throw new Error(`News API call failed with status ${response.status}`);
+  }
 
   return {
-    newsPromise: new Promise(() => {})
+    newsItem: newsItemSchema.parse(await response.json())
   };
 };
