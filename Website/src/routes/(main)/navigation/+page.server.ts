8c37020d4abb7d55f89dc83689d414358abefd50
Jay Malhotra <jay0@me.com>
2024-06-29 21:49:40 +0100

    Add lint CI job

---
+++ Website/src/routes/(main)/navigation/+page.server.ts
@@ -0,0 +1,5 @@
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = ({ locals }) => ({
+  hasValidJwt: locals.hasValidJwt
+});
--- Website/src/routes/(main)/navigation/+page@.svelte
+++ Website/src/routes/(main)/navigation/+page@.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
   import Routes from '$main/routes.svelte';
+
+  import type { PageData } from './$types';
+
+  export let data: PageData;
 </script>
 
 <nav class="flex flex-col justify-center gap-1 p-3">
   <h1 class="mb-4 text-3xl font-bold">Navigation</h1>
-  <Routes />
+  <Routes hasValidJwt={data.hasValidJwt} />
 </nav>
