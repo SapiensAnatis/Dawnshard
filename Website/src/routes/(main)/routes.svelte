8a66a6e30ee08aedf7d4bf5ecbe3dabab6ccf0cd
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2024-07-02 20:39:24 +0100

    Fix drawer
    Use an ugly workaround in which the nav buttons are wrapped in Drawer.Close onditionally to avoid having to programmatically control open. Programmatically controlling open causes issues:
    
    - the drawer close animation doesn't play; it just disappears instantly
    - the drawer close momentarily resets scroll on iOS/safari
    
    Also add meta tags for discord embed, and remove second image tag

--- Website/src/routes/(main)/routes.svelte
+++ Website/src/routes/(main)/routes.svelte
@@ -1,33 +1,28 @@
 <script lang="ts">
-  import { createEventDispatcher } from 'svelte';
+  import { Close } from '$shadcn/components/ui/drawer';
 
-  import { Button } from '$shadcn/components/ui/button';
-
+  import RouteButton from './routeButton.svelte';
   import { routeGroups } from './routes.ts';
 
   export let hasValidJwt: boolean;
-
-  const dispatch = createEventDispatcher();
-
-  const onClick = () => {
-    dispatch('navigate');
-  };
+  export let drawer: boolean = false;
 </script>
 
 {#each routeGroups as routeGroup}
   {#if !routeGroup.requireAuth || (routeGroup.requireAuth && hasValidJwt)}
     <div>
       <h2 class="mb-1 scroll-m-20 text-lg font-semibold tracking-tight">{routeGroup.title}</h2>
-      {#each routeGroup.routes as route}
-        <Button
-          href={route.href}
-          variant="ghost"
-          class="h-11 w-[90%] justify-start"
-          on:click={onClick}>
-          <svelte:component this={route.icon} class="mr-2 size-6" aria-hidden="true" />
-          {route.title}
-        </Button>
-      {/each}
+      {#if drawer}
+        <Close class="w-full flex flex-col">
+          {#each routeGroup.routes as route}
+            <RouteButton {route} />
+          {/each}
+        </Close>
+      {:else}
+        {#each routeGroup.routes as route}
+          <RouteButton {route} />
+        {/each}
+      {/if}
     </div>
   {/if}
 {/each}
