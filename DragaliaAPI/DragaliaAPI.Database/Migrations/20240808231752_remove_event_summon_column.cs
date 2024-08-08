78f2815d1eac6ea14c545c0be3b96690c3ea2d8c
Jay Malhotra <jay0@me.com>
2024-08-09 00:18:17 +0100

    remove dumb column

---
+++ DragaliaAPI/DragaliaAPI.Database/Migrations/20240808231752_remove_event_summon_column.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DragaliaAPI.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class remove_event_summon_column : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BoxSummonCount",
+                table: "PlayerEventSummonData");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "BoxSummonCount",
+                table: "PlayerEventSummonData",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
