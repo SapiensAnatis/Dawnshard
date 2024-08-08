c65626b4c22a2f61682db9ddc3553946a3f160d1
Jay Malhotra <jay0@me.com>
2024-08-09 00:18:17 +0100

    remove dumb column

--- DragaliaAPI/DragaliaAPI.Database/Migrations/20240808231752_remove_event_summon_column.Designer.cs
+++ DragaliaAPI/DragaliaAPI.Database/Migrations/20240808231752_remove_event_summon_column.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DragaliaAPI.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DragaliaAPI.Database.Migrations
 {
     [DbContext(typeof(ApiContext))]
-    partial class ApiContextModelSnapshot : ModelSnapshot
+    [Migration("20240808231752_remove_event_summon_column")]
+    partial class remove_event_summon_column
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
