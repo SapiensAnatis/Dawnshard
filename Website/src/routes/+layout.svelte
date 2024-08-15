2dff652d85aabeff514721c161cc0da49839e126
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2024-08-15 21:52:09 +0100

    More nerdbank (#1022)
    - Use nerdbank for website images
    - Make main branch beta tags (release versions come from Git tags, if we
    ever use those again)
    - Add docker label images
    - Add dependabot to website docker file

--- Website/src/routes/+layout.svelte
+++ Website/src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
   import '../app.pcss';
+
+  // @ts-expect-error __APP_VERSION__ is replaced by svelte-preprocess
+  // eslint-disable-next-line no-undef
+  const version = __APP_VERSION__;
 </script>
+
+<svelte:head>
+  <meta property="version" content={version} />
+</svelte:head>
 
 <slot />
