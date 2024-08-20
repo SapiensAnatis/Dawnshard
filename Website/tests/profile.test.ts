95aa1bf79611442c47637b24c99ece32cd7e6de9
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2024-08-20 20:38:38 +0100

    Add global auth setup

--- Website/tests/profile.test.ts
+++ Website/tests/profile.test.ts
@@ -3,8 +3,6 @@ import { expect, type Page, test } from '@playwright/test';
 test('displays correctly', async ({ page }) => {
   await page.goto('/');
 
-  await login(page);
-
   const profileLink = page.getByRole('link', { name: 'Profile' });
   await expect(profileLink).toBeVisible();
   await profileLink.click();
@@ -16,8 +14,6 @@ test('displays correctly', async ({ page }) => {
 
 test('export save', async ({ page }) => {
   await page.goto('/');
-
-  await login(page);
 
   const profileLink = page.getByRole('link', { name: 'Profile' });
   await expect(profileLink).toBeVisible();
@@ -35,25 +31,3 @@ test('export save', async ({ page }) => {
 
   expect(text).toBe('{"someData":"true"}');
 });
-
-const login = async (page: Page) => {
-  if (!process.env.BAAS_USERNAME || !process.env.BAAS_PASSWORD) {
-    throw new Error(
-      'This test needs the environment variables BAAS_USERNAME and BAAS_PASSWORD to be set.'
-    );
-  }
-
-  await page.getByRole('link', { name: 'Login' }).click();
-
-  // Begin baas.lukefz.xyz
-  await page.waitForTimeout(5000); // If removed, buttons don't work. Maybe need to wait for Blazor to initialize?
-  await page.getByText('Login', { exact: true }).click();
-
-  await page.locator('css=#username').fill(process.env.BAAS_USERNAME);
-  await page.locator('css=#password').fill(process.env.BAAS_PASSWORD);
-  await page.getByRole('button', { name: 'Login' }).click();
-
-  await expect(page.getByRole('heading', { name: 'Login using Dragalia' })).toBeVisible();
-  await page.getByRole('button', { name: 'Login' }).click();
-  // End baas.lukefz.xyz
-};
