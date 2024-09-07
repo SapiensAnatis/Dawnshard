bd0e0018ae104591f8ea739777b3e3be9da8402f
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2024-09-07 22:03:47 +0100

    Fix navbar buttons not taking full width on ios safari (#1067)

--- Website/src/routes/(main)/routeButton.svelte
+++ Website/src/routes/(main)/routeButton.svelte
@@ -5,7 +5,7 @@
   export let route: Route;
 </script>
 
-<Button href={route.href} variant="ghost" class="flex h-11  justify-start">
+<Button href={route.href} variant="ghost" class="flex h-11 w-full justify-start">
   <svelte:component this={route.icon} class="mr-2 size-6 shrink-0 basis-6" aria-hidden="true" />
   <span>{route.title}</span>
 </Button>
