7b180beefc34deb75e1880b1bd4882f09102ab7f
Jay Malhotra <jay0@me.com>
2024-12-31 16:08:44 +0000

    Enable .NET Aspire orchestration for Photon

--- Aspire/Dawnshard.AppHost/Program.cs
+++ Aspire/Dawnshard.AppHost/Program.cs
@@ -1,3 +1,5 @@
+using System.Net.Sockets;
+using System.Security.Cryptography;
 using Microsoft.Extensions.Configuration;
 
 IDistributedApplicationBuilder builder = DistributedApplication.CreateBuilder(args);
@@ -15,17 +17,83 @@ IResourceBuilder<ProjectResource> dragaliaApi = builder
     .AddProject<Projects.DragaliaAPI>("dragalia-api")
     .WithReference(postgres)
     .WithReference(redis)
+    .WithEndpoint("http", http => http.TargetHost = "0.0.0.0")
     .WithExternalHttpEndpoints();
 
-if (builder.Configuration.GetValue<bool>("EnableStateManager"))
+if (builder.Configuration.GetValue<bool>("EnablePhoton"))
 {
+    string bearerToken = Convert.ToBase64String(RandomNumberGenerator.GetBytes(32));
+
     IResourceBuilder<ProjectResource> stateManager = builder
         .AddProject<Projects.DragaliaAPI_Photon_StateManager>("photon-state-manager")
         .WithReference(redis)
         .WithEndpoint("http", http => http.TargetHost = "0.0.0.0")
-        .WithExternalHttpEndpoints();
+        .WithExternalHttpEndpoints()
+        .WithEnvironment("PhotonOptions__Token", bearerToken);
 
-    dragaliaApi.WithEnvironment("PhotonOptions__StateManagerUrl", stateManager.GetEndpoint("http"));
+    // This can't be configured here, but you also need to edit appsettings.json to set PhotonOptions:Url to the address
+    // of your PC, port 5055, as seen from your mobile device. So if using an actual device and connecting over your
+    // local IP, set it to e.g. 192.168.1.109:5055
+    dragaliaApi = dragaliaApi
+        .WithEnvironment("PhotonOptions__StateManagerUrl", stateManager.GetEndpoint("http"))
+        .WithEnvironment("PhotonOptions__Token", bearerToken);
+
+    // Corresponds to PhotonServer/ directory in repo root
+    string photonServerDirectory = Path.Combine(
+        AppContext.BaseDirectory,
+        "..",
+        "..",
+        "..",
+        "..",
+        "..",
+        "PhotonServer",
+        "deploy",
+        "bin_Win64"
+    );
+
+    IResourceBuilder<ExecutableResource> photonServer = builder
+        .AddExecutable(
+            "photon-server",
+            Path.Combine(photonServerDirectory, "PhotonSocketServer.exe"),
+            photonServerDirectory,
+            "/run",
+            "LoadBalancing",
+            "/configPath",
+            photonServerDirectory
+        )
+        .WithEnvironment("ApiServerUrl", dragaliaApi.GetEndpoint("http"))
+        .WithEnvironment("StateManagerUrl", stateManager.GetEndpoint("http"))
+        .WithEnvironment("BearerToken", bearerToken)
+        .WithEndpoint(
+            "gs-to-ms", // https://doc.photonengine.com/server/current/operations/tcp-and-udp-port-numbers
+            e =>
+            {
+                e.Port = 4520;
+                e.IsProxied = false;
+                e.IsExternal = true;
+                e.Protocol = ProtocolType.Tcp;
+            }
+        )
+        .WithEndpoint(
+            "client-to-ms",
+            e =>
+            {
+                e.Port = 5055;
+                e.IsProxied = false;
+                e.IsExternal = true;
+                e.Protocol = ProtocolType.Udp;
+            }
+        )
+        .WithEndpoint(
+            "client-to-gs",
+            e =>
+            {
+                e.Port = 5056;
+                e.IsProxied = false;
+                e.IsExternal = true;
+                e.Protocol = ProtocolType.Udp;
+            }
+        );
 }
 
 if (builder.Configuration.GetValue<bool>("EnableWebsite"))
