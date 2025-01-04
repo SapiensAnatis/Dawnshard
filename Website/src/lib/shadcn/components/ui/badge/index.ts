a8b9d076126793fa5188f4d04832b1fb4886039f
Jay Malhotra <jay0@me.com>
2025-01-04 13:43:50 +0000

    Revert "fix ts errors"
    This reverts commit 89c6e48212bfb5bdab7ffcb7c16685ffe3b73cc9.

--- Website/src/lib/shadcn/components/ui/badge/index.ts
+++ Website/src/lib/shadcn/components/ui/badge/index.ts
@@ -1,3 +1,2 @@
 export { default as Badge } from "./badge.svelte";
-// @ts-expect-error unable to import BadgeVariant https://github.com/huntabyte/shadcn-svelte/issues/1468
 export { badgeVariants, type BadgeVariant } from "./badge.svelte";
--- Website/src/lib/shadcn/components/ui/button/index.ts
+++ Website/src/lib/shadcn/components/ui/button/index.ts
@@ -1,11 +1,7 @@
 import Root, {
-	// @ts-expect-error unable to import ButtonProps https://github.com/huntabyte/shadcn-svelte/issues/1468
 	type ButtonProps,
-	// @ts-expect-error unable to export ButtonSize
 	type ButtonSize,
-	// @ts-expect-error unable to export ButtonVariant
 	type ButtonVariant,
-	// @ts-expect-error unable to import buttonVariants
 	buttonVariants,
 } from "./button.svelte";
 
