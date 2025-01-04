96e890998b2b24ec86d8dd0e49c767dfc747c043
Jay Malhotra <jay0@me.com>
2025-01-04 21:26:59 +0000

    Fix login bugs
    - Fix repeated "can't get challenge string" errors - I suspect this is bots or people otherwise directly browsing to /ouath. Instead, return a plain bad request if visiting with wrong parameters.
    - Store the original page when redirecting to /unauthorized and use it when logging in - this will allow direct linking to e.g. /account/save-editor
    
    # Conflicts:
    #	Website/src/routes/(main)/headerContents.svelte

--- Website/src/routes/(main)/headerContents.svelte
+++ Website/src/routes/(main)/headerContents.svelte
@@ -4,23 +4,32 @@
   import { toggleMode } from 'mode-watcher';
   import { onMount } from 'svelte';
 
-  import { page } from '$app/stores';
+  import { page } from '$app/state';
   import { Button } from '$shadcn/components/ui/button';
 
-  let jsLoaded = false;
+  const getOriginalPage = () => {
+    const searchParam = page.url.searchParams.get('originalPage');
+    if (searchParam) {
+      return searchParam; // already URL encoded
+    }
 
-  export let hasValidJwt: boolean;
+    return encodeURIComponent(page.url.pathname);
+  };
+
+  let initialized = $state(false);
+  let originalPage = getOriginalPage();
+  let { hasValidJwt }: { hasValidJwt: boolean } = $props();
 
   onMount(() => {
     // We use this in the tests to delay clicking on the theme toggle until mode-watcher is likely
     // to be ready to work.
-    jsLoaded = true;
+    initialized = true;
   });
 </script>
 
 <h1 class="scroll-m-20 text-2xl font-bold tracking-tight md:text-3xl">Dawnshard</h1>
 <div class="flex-grow"></div>
-<Button onclick={toggleMode} variant="outline" size="icon" data-loaded={jsLoaded}>
+<Button onclick={toggleMode} variant="outline" size="icon" data-loaded={initialized}>
   <Sun
     class="absolute size-8 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
     size="400"
@@ -34,7 +43,7 @@
 {#if hasValidJwt}
   <Button href="/logout" variant="secondary" data-sveltekit-reload>Log out</Button>
 {:else}
-  <Button href={`/login?originalPage=${$page.url.pathname}`} data-sveltekit-preload-data="off">
+  <Button href={`/login?originalPage=${originalPage}`} data-sveltekit-preload-data="off">
     Login
   </Button>
 {/if}
