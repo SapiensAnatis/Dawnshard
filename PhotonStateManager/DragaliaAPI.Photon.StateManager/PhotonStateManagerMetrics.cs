6d30d14e561b68c34672b3ba8f436627983ca39f
Jay Malhotra <jay0@me.com>
2025-01-18 12:04:30 +0000

    Fix room count metric

--- PhotonStateManager/DragaliaAPI.Photon.StateManager/PhotonStateManagerMetrics.cs
+++ PhotonStateManager/DragaliaAPI.Photon.StateManager/PhotonStateManagerMetrics.cs
@@ -25,30 +25,18 @@ public class PhotonStateManagerMetrics
 
     private IEnumerable<Measurement<int>> GetRoomCount()
     {
-        List<RedisGame> games = this.connectionProvider.RedisCollection<RedisGame>().ToList();
+        int publicRooms = this
+            .connectionProvider.RedisCollection<RedisGame>()
+            .Count(x => x.MatchingType == MatchingTypes.Anyone);
 
-        List<Measurement<int>> measurements = games
-            .GroupBy(
-                RoomTags.Create,
-                (tags, rooms) => new Measurement<int>(rooms.Count(), tags.ToTagList())
-            )
-            .DefaultIfEmpty(new Measurement<int>(0))
-            .ToList();
+        int privateRooms = this
+            .connectionProvider.RedisCollection<RedisGame>()
+            .Count(x => x.MatchingType != MatchingTypes.Anyone);
 
-        return measurements;
+        return
+        [
+            new Measurement<int>(publicRooms, [new("room.public", true)]),
+            new Measurement<int>(privateRooms, [new("room.public", false)]),
+        ];
     }
-}
-
-file record RoomTags(MatchingTypes MatchingType, int PlayerCount, bool Visible)
-{
-    public static RoomTags Create(RedisGame game) =>
-        new(game.MatchingType, game.Players.Count, game.Visible);
-
-    public TagList ToTagList() =>
-        new()
-        {
-            new("room.matching_type", this.MatchingType.ToString()),
-            new("room.player_count", this.PlayerCount),
-            new("room.visible", this.Visible),
-        };
 }
