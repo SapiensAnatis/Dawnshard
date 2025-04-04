1acb125cf27fd2525d66cf5c794df43086db8c42
Jay Malhotra <jay0@me.com>
2025-04-04 23:23:15 +0100

    Add remaining facility event missions
    Adds the remaining event compendium endeavours for facility events.
    
    I had a go at generating these using Gemini 2.5. I fed it some sample mission lists and some text files with data from the master asset. I had to fix about 5 missions out of ~125 in the end, but it was otherwise pretty much spot on!

---
+++ DragaliaAPI/DragaliaAPI.MissionDesigner/Missions/MemoryEvent/ACrescendoOfCourage.cs
@@ -0,0 +1,87 @@
+/* This file was AI-generated */
+
+using DragaliaAPI.MissionDesigner.Models;
+using DragaliaAPI.MissionDesigner.Models.Attributes;
+using DragaliaAPI.MissionDesigner.Models.EventMission;
+using DragaliaAPI.MissionDesigner.Models.RegularMission; // For ClearQuestMission
+
+namespace DragaliaAPI.MissionDesigner.Missions.MemoryEvent;
+
+[ContainsMissionList]
+public static class CrescendoOfCourage
+{
+    // Quest IDs from QuestNames.txt
+    private const int ExtraBossBattleId = 208290401; // A Fiendish Encore
+
+    [MissionType(MissionType.MemoryEvent)]
+    [EventId(20829)]
+    public static List<Mission> Missions { get; } =
+        [
+            // Participate in the Event
+            new EventParticipationMission() { MissionId = 10070101 },
+            // Clear a Boss Battle
+            new EventRegularBattleClearMission() { MissionId = 10070102 },
+            // Clear an "A Crescendo of Courage" Quest with Surfing Siblings Equipped
+            new EventQuestClearWithCrestMission()
+            {
+                MissionId = 10070201,
+                Crest = AbilityCrestId.SurfingSiblings,
+            },
+            // Collect 100 Hype in One Go
+            new EventPointCollectionRecordMission() { MissionId = 10070301 },
+            // Collect 500 Hype in One Go
+            new EventPointCollectionRecordMission() { MissionId = 10070302 },
+            // Collect 1,500 Hype in One Go
+            new EventPointCollectionRecordMission() { MissionId = 10070303 },
+            // Collect 4,000 Hype in One Go
+            new EventPointCollectionRecordMission() { MissionId = 10070304 },
+            // Collect 7,000 Hype in One Go
+            new EventPointCollectionRecordMission() { MissionId = 10070305 },
+            // Clear Five Boss Battles
+            new EventRegularBattleClearMission() { MissionId = 10070401 },
+            // Clear 10 Boss Battles
+            new EventRegularBattleClearMission() { MissionId = 10070402 },
+            // Clear 20 Boss Battles
+            new EventRegularBattleClearMission() { MissionId = 10070403 },
+            // Clear 30 Boss Battles
+            new EventRegularBattleClearMission() { MissionId = 10070404 },
+            // Clear Three Extra Boss Battles
+            new ClearQuestMission() { MissionId = 10070501, QuestId = ExtraBossBattleId },
+            // Clear Six Extra Boss Battles
+            new ClearQuestMission() { MissionId = 10070502, QuestId = ExtraBossBattleId },
+            // Clear 10 Extra Boss Battles
+            new ClearQuestMission() { MissionId = 10070503, QuestId = ExtraBossBattleId },
+            // Clear 15 Extra Boss Battles
+            new ClearQuestMission() { MissionId = 10070504, QuestId = ExtraBossBattleId },
+            // Clear Three Challenge Battles
+            new EventChallengeBattleClearMission() { MissionId = 10070601 },
+            // Clear Six Challenge Battles
+            new EventChallengeBattleClearMission() { MissionId = 10070602 },
+            // Clear 10 Challenge Battles
+            new EventChallengeBattleClearMission() { MissionId = 10070603 },
+            // Clear 15 Challenge Battles
+            new EventChallengeBattleClearMission() { MissionId = 10070604 },
+            // Completely Clear a Challenge Battle on Expert
+            new EventChallengeBattleClearMission()
+            {
+                MissionId = 10070701,
+                FullClear = true,
+                VariationType = VariationTypes.VeryHard, // Difficulty 3 for Expert Challenge Battle
+            },
+            // Completely Clear a Challenge Battle on Master
+            new EventChallengeBattleClearMission()
+            {
+                MissionId = 10070801,
+                FullClear = true,
+                VariationType = VariationTypes.Extreme, // Difficulty 4 for Master Challenge Battle
+            },
+            // Earn the "Summer Champion" Epithet
+            // Typically awarded for completing the Master Challenge Battle with all endeavors
+            new EventChallengeBattleClearMission()
+            {
+                MissionId = 10070901,
+                FullClear = true,
+                VariationType = VariationTypes.Extreme, // Linked to Master Challenge Battle Clear
+            },
+        ];
+}
---
+++ DragaliaAPI/DragaliaAPI.MissionDesigner/Missions/MemoryEvent/ADashOfDisaster.cs
@@ -0,0 +1,83 @@
+/* This file was AI-generated */
+
+using DragaliaAPI.MissionDesigner.Models;
+using DragaliaAPI.MissionDesigner.Models.Attributes;
+using DragaliaAPI.MissionDesigner.Models.EventMission;
+
+namespace DragaliaAPI.MissionDesigner.Missions.MemoryEvent;
+
+[ContainsMissionList]
+public static class ADashOfDisaster
+{
+    [MissionType(MissionType.MemoryEvent)]
+    [EventId(20841)]
+    public static List<Mission> Missions { get; } =
+        [
+            // Participate in the Event
+            new EventParticipationMission() { MissionId = 10180101 },
+            // Clear a Boss Battle
+            new EventRegularBattleClearMission() { MissionId = 10180201 },
+            // Clear an "A Dash of Disaster" Quest with Berry Lovable Friends Equipped
+            new EventQuestClearWithCrestMission()
+            {
+                MissionId = 10180401,
+                Crest = AbilityCrestId.BerryLovableFriends,
+            },
+            // Collect 100 Zest in One Go
+            new EventPointCollectionRecordMission() { MissionId = 10180501 },
+            // Collect 500 Zest in One Go
+            new EventPointCollectionRecordMission() { MissionId = 10180502 },
+            // Collect 1,500 Zest in One Go
+            new EventPointCollectionRecordMission() { MissionId = 10180503 },
+            // Collect 4,000 Zest in One Go
+            new EventPointCollectionRecordMission() { MissionId = 10180504 },
+            // Collect 6,000 Zest in One Go
+            new EventPointCollectionRecordMission() { MissionId = 10180505 },
+            // Collect 7,000 Zest in One Go
+            new EventPointCollectionRecordMission() { MissionId = 10180506 },
+            // Clear Five Boss Battles
+            new EventRegularBattleClearMission() { MissionId = 10180601 },
+            // Clear 10 Boss Battles
+            new EventRegularBattleClearMission() { MissionId = 10180602 },
+            // Clear 15 Boss Battles
+            new EventRegularBattleClearMission() { MissionId = 10180603 },
+            // Clear 20 Boss Battles
+            new EventRegularBattleClearMission() { MissionId = 10180604 },
+            // Clear 30 Boss Battles
+            new EventRegularBattleClearMission() { MissionId = 10180605 },
+            // Clear Three Challenge Battles
+            new EventChallengeBattleClearMission() { MissionId = 10180801 },
+            // Clear Six Challenge Battles
+            new EventChallengeBattleClearMission() { MissionId = 10180802 },
+            // Clear 10 Challenge Battles
+            new EventChallengeBattleClearMission() { MissionId = 10180803 },
+            // Clear 15 Challenge Battles
+            new EventChallengeBattleClearMission() { MissionId = 10180804 },
+            // Clear 20 Challenge Battles
+            new EventChallengeBattleClearMission() { MissionId = 10180805 },
+            // Clear 25 Challenge Battles
+            new EventChallengeBattleClearMission() { MissionId = 10180806 },
+            // Completely Clear a Challenge Battle on Expert
+            new EventChallengeBattleClearMission()
+            {
+                MissionId = 10180901,
+                FullClear = true,
+                VariationType = VariationTypes.VeryHard, // Difficulty 3 for Expert Challenge Battle
+            },
+            // Completely Clear a Challenge Battle on Master
+            new EventChallengeBattleClearMission()
+            {
+                MissionId = 10181001,
+                FullClear = true,
+                VariationType = VariationTypes.Extreme, // Difficulty 4 for Master Challenge Battle
+            },
+            // Earn the "Chef de Cuisine" Epithet
+            // Typically awarded for completing the Master Challenge Battle with all endeavors
+            new EventChallengeBattleClearMission()
+            {
+                MissionId = 10181301,
+                FullClear = true,
+                VariationType = VariationTypes.Extreme, // Linked to Master Challenge Battle Clear
+            },
+        ];
+}
