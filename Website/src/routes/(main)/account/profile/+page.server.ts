c595132b9d93f7c0c9f5d303dafa9ef910da0530
Jay Malhotra <jay0@me.com>
2025-04-27 12:28:57 +0100

    Fix disabling settings
    When a switch is toggled off, it is absent from the form data. This leads to it being defaulted to true, meaning it is impossible to toggle off. Iterate a known set of setting keys so that our request object contains all of them with true/false values. Additionally introduce validation that all keys are in the request object with JsonRequired.

--- Website/src/routes/(main)/account/profile/+page.server.ts
+++ Website/src/routes/(main)/account/profile/+page.server.ts
@@ -1,4 +1,8 @@
+import { userProfileSchema } from '$main/account/profile/userProfile.ts';
+
 import type { Actions } from './$types';
+
+const settingKeys = Object.keys(userProfileSchema.shape.settings.shape) as const;
 
 export const actions = {
   settings: async ({ request, fetch, url }) => {
@@ -7,8 +11,8 @@ export const actions = {
     let requestObject = {};
 
     // Convert values to actual booleans
-    for (const [key, value] of Object.entries(dataObject)) {
-      requestObject = { ...requestObject, [key]: value === 'on' };
+    for (const key of settingKeys) {
+      requestObject = { ...requestObject, [key]: data.get(key) === 'on' };
     }
 
     const fetchRequest = new Request(new URL('/api/settings', url.origin), {
@@ -26,6 +30,6 @@ export const actions = {
       throw new Error(`Failed to save settings: status ${response.status}`);
     }
 
-    return { ...requestObject };
+    return { ...dataObject };
   }
 } satisfies Actions;
