517698893604d954f3569e5540e7f54feccf934e
Jay Malhotra <jay0@me.com>
2025-04-27 11:43:31 +0100

    Fix tests

--- Website/tests/profile.test.ts
+++ Website/tests/profile.test.ts
@@ -56,27 +56,27 @@ test('change settings', async ({ page }) => {
   const form = page.getByRole('form', { name: 'Settings' });
 
   await expect(form.getByRole('switch', { name: 'Receive daily material gifts' })).toBeEnabled();
-  await expect(
-    form.getByRole('switch', { name: 'Receive daily material gifts' })
-  ).not.toBeChecked();
+  await expect(form.getByRole('switch', { name: 'Receive daily material gifts' })).toBeChecked(); // Default is enabled
   await expect(form.getByRole('button', { name: 'Reset' })).toBeDisabled();
   await expect(form.getByRole('button', { name: 'Save' })).toBeDisabled();
 
   // Submit
   await form.getByRole('switch', { name: 'Receive daily material gifts' }).click();
-  await expect(form.getByRole('switch', { name: 'Receive daily material gifts' })).toBeChecked();
+  await expect(
+    form.getByRole('switch', { name: 'Receive daily material gifts' })
+  ).not.toBeChecked();
   await expect(form.getByRole('button', { name: 'Save' })).toBeEnabled();
   await form.getByRole('button', { name: 'Save' }).click();
   await expect(page.getByText('Successfully changed settings')).toBeVisible();
 
   // Reset
   await form.getByRole('switch', { name: 'Receive daily material gifts' }).click();
+  await expect(form.getByRole('switch', { name: 'Receive daily material gifts' })).toBeChecked();
+  await expect(form.getByRole('button', { name: 'Reset' })).toBeEnabled();
+  await form.getByRole('button', { name: 'Reset' }).click();
   await expect(
     form.getByRole('switch', { name: 'Receive daily material gifts' })
   ).not.toBeChecked();
-  await expect(form.getByRole('button', { name: 'Reset' })).toBeEnabled();
-  await form.getByRole('button', { name: 'Reset' }).click();
-  await expect(form.getByRole('switch', { name: 'Receive daily material gifts' })).toBeChecked();
   await expect(form.getByRole('button', { name: 'Reset' })).toBeDisabled();
   await expect(form.getByRole('button', { name: 'Save' })).toBeDisabled();
 });
