16ba18667e714553e94c5bb2aeb166dd467c625d
Jay Malhotra <jay0@me.com>
2025-05-11 22:19:57 +0100

    Current work

--- DragaliaAPI/DragaliaAPI/Infrastructure/Hangfire/HangfireExtensions.cs
+++ DragaliaAPI/DragaliaAPI/Infrastructure/Hangfire/HangfireExtensions.cs
@@ -8,10 +8,17 @@ public static class HangfireExtensions
     public static WebApplication AddHangfireJobs(this WebApplication app)
     {
         IRecurringJobManager jobClient = app.Services.GetRequiredService<IRecurringJobManager>();
+
         jobClient.AddOrUpdate(
             SummonHistoryJob.Id,
             Job.FromExpression<SummonHistoryJob>(s => s.PurgeSummonHistory()),
             Cron.Daily(01, 00)
+        );
+
+        jobClient.AddOrUpdate(
+            HelperUseDateJob.Id,
+            Job.FromExpression<HelperUseDateJob>(s => s.PurgeHelperUseDate()),
+            Cron.Daily(06, 00) // daily reset
         );
 
         return app;
