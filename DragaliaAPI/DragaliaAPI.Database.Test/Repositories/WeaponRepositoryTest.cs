0be7e5186db3e2a16d9f276fa02fbfea1e902aa0
Jay Malhotra <jay0@me.com>
2025-05-16 23:53:36 +0100

    Better weapon skin error

--- DragaliaAPI/DragaliaAPI.Database.Test/Repositories/WeaponRepositoryTest.cs
+++ DragaliaAPI/DragaliaAPI.Database.Test/Repositories/WeaponRepositoryTest.cs
@@ -194,7 +194,9 @@ public class WeaponRepositoryTest : IClassFixture<DbTestFixture>
         await this.fixture.ApiContext.SaveChangesAsync(TestContext.Current.CancellationToken);
 
         this.fixture.ApiContext.PlayerWeaponSkins.Should()
-            .ContainEquivalentOf(
+            .Contain(x => x.WeaponSkinId == 4)
+            .Which.Should()
+            .BeEquivalentTo(
                 new DbWeaponSkin()
                 {
                     ViewerId = IdentityTestUtils.ViewerId,
