7f6d0f270c18f276b3a36566ee745e1677a2d2aa
Jay Malhotra <jay0@me.com>
2026-02-20 21:34:58 +0000

    csharpier

--- DragaliaAPI/DragaliaAPI/Features/Web/News/NewsController.cs
+++ DragaliaAPI/DragaliaAPI/Features/Web/News/NewsController.cs
@@ -19,7 +19,7 @@ internal sealed class NewsController(NewsService newsService) : ControllerBase
             request.PageSize,
             cancellationToken
         );
-        
+
         data = data.Select(this.DoTemplateSubstitutions).ToList();
 
         int totalCount = await newsService.GetNewsItemCountAsync(cancellationToken);
@@ -46,7 +46,10 @@ internal sealed class NewsController(NewsService newsService) : ControllerBase
 
     private NewsItem DoTemplateSubstitutions(NewsItem newsItem)
     {
-        return newsItem with { Description = newsItem.Description.Replace("{{Hostname}}", this.Request.Host.Host) };
+        return newsItem with
+        {
+            Description = newsItem.Description.Replace("{{Hostname}}", this.Request.Host.Host),
+        };
     }
 }
 
